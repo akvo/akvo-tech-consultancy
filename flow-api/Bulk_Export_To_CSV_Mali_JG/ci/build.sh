88b587b39e4ab45209880740bb5387e88ab30a84
Dan Lebrero <dlebrero@gmail.com>
2018-07-06 13:50:47 +0200

    setting correct directory in .travis.yml so that we do not need to repeat ourselves

--- flow-api/Bulk_Export_To_CSV_Mali_JG/ci/build.sh
+++ flow-api/Bulk_Export_To_CSV_Mali_JG/ci/build.sh
@@ -11,9 +11,6 @@ if [ -z "$TRAVIS_COMMIT" ]; then
     export TRAVIS_COMMIT=local
 fi
 
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ..
-
 log Creating Production image
 
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/tech-consultancy-export-csv-mali:${TRAVIS_COMMIT} .
