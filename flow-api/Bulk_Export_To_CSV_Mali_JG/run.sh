a9570ec6a6256da1d411e81ad390c7119e589089
Dan Lebrero <dlebrero@gmail.com>
2018-07-13 10:35:12 +0200

    testing upload to bucket

--- flow-api/Bulk_Export_To_CSV_Mali_JG/run.sh
+++ flow-api/Bulk_Export_To_CSV_Mali_JG/run.sh
@@ -5,6 +5,13 @@ set -eu
 echo "Starting"
 #python3 FlowApi.py akvoflowsandbox survey 25793011
 
+date >> upload.txt
+
 echo "Done"
+
+gcloud auth activate-service-account --key-file=/var/run/secret/cloud.google.com/service-account.json
+gcloud config set project akvo-lumen
+
+gsutil cp upload.txt gs://tech-consultancy/mali
 
 exit 1
