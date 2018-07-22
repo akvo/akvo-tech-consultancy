173424abe94546687ee475112141c5d96f7c3923
dedenbangkit <mail@dedenbangkit.com>
2018-07-23 04:37:22 +0800

    specific trigger function

--- flow-api/Jobs_GreenCoffee_TriggerUpdate/app.py
+++ flow-api/Jobs_GreenCoffee_TriggerUpdate/app.py
@@ -2,12 +2,111 @@ from flask import Flask, jsonify, request
 from datetime import datetime
 from app.config import requestURI, postURI, keymd5, payload, accounts, headers
 from app.api import getResponse, getForm, setQuestionAttr, setData
+from pytz import utc, timezone
 import xmltodict
 import requests
 import dateutil.parser
 import pandas as pd
+import logging
 from collections import OrderedDict
 app = Flask(__name__)
+log_file = './error-log.log'
+logging.basicConfig(filename=log_file,level=logging.DEBUG)
+
+def filterAnswerbyDate(dt):
+    if dt.date() == datetime.today().date():
+        return True
+    return False
+
+def filterData(answer, par_id, meta, submitter_id):
+    resp = [
+        {'key':'keymd5', 'value':keymd5},
+        {'key':'ACC_ID', 'value': submitter_id},
+    ]
+    for mt in meta:
+        try:
+            values = answer['responses'][par_id][0][mt['id']]
+            if mt['type'] == 'CASCADE':
+                resp.append({'key':'ID_Commodity', 'value':values[2]['code']})
+                resp.append({'key':'ID_Agency', 'value':values[1]['code']})
+            elif mt['type'] == 'DATE':
+                date_val = datetime.strptime(values, '%Y-%m-%dT%H:%M:%S.%fZ')
+                date_here = utc.localize(date_val).astimezone(timezone('Asia/Singapore'))
+                check_date = filterAnswerbyDate(date_here)
+                if check_date:
+                    new_date = datetime.strftime(date_here,'%Y/%m/%d')
+                    resp.append({'key':'Date_var', 'value':new_date})
+                else:
+                    return False
+            else:
+                resp.append({'key':mt['variableName'],'value':str(values)})
+        except:
+            resp.append({'key':mt['variableName'],'value':'0'})
+    return resp
+
+def getRawData(survey_id, form_id):
+    collections = []
+    data = getResponse(requestURI + '/surveys/' + survey_id)
+    # submitter ID
+    submitter_id = data['name'].split('_')[1]
+    acc_detail = None
+    for account in accounts:
+        if account['id'] == submitter_id:
+            acc_detail = account
+    meta = data['forms'][0]['questionGroups'][0]['questions']
+    for form in data['forms']:
+        par_id = form['questionGroups'][0]['id']
+        answers = getResponse(requestURI + '/form_instances?survey_id=' + survey_id + '&form_id=' + form_id)
+        for answer in answers['formInstances']:
+            filteredDate = filterData(answer,par_id, meta, submitter_id)
+            if filteredDate:
+                login = payload([
+                    {'key':'keymd5', 'value':keymd5},
+                    {'key':'acc_name', 'value':acc_detail['name']},
+                    {'key':'acc_pass', 'value':acc_detail['pass']}
+                ])
+                url = postURI + '/login_return_ACC_ID'
+                u = requests.post(url, data = login, headers = headers)
+                uid = xmltodict.parse(u.text)
+                ids = uid['short']['#text']
+                if ids == '0':
+                    log = 'error login'
+                    logging.warn(log)
+                else:
+                    url = postURI + '/add_price_return_newid'
+                    r = requests.post(url, data = payload(filteredDate), headers = headers)
+                    try:
+                        log = xmltodict.parse(r.text)
+                        log = 'Success! ' + log['string']['#text']
+                        logging.info(log)
+                    except xmltodict.expat.ExpatError:
+                        log = r.text
+                        logging.error(log)
+                collections.append({
+                    'id': answer['id'],
+                    'submitter':answer['submitter'],
+                    'identifier':answer['identifier'],
+                    'device':answer['deviceIdentifier'],
+                    'submited_at':answer['submissionDate'],
+                    'login':acc_detail,
+                    'payload':payload(filteredDate),
+                })
+    return collections
+
+@app.route('/greencoffee')
+def updateAll():
+    data = getResponse(requestURI + '/surveys?folder_id=38000001')
+    lists = []
+    for dt in data['surveys']:
+        sid = getResponse(requestURI + '/surveys/' + dt['id'])
+        responses = getRawData(dt['id'], sid['forms'][0]['id'])
+        if responses is not None:
+            for resp in responses:
+                lists.append(resp)
+    return jsonify(lists)
+
+
+### Pragmatic / Manual Trigger
 
 @app.route('/folders')
 def getFolder():
@@ -36,6 +135,7 @@ def getSurveys(id):
         lists.append(list)
     return jsonify(lists)
 
+
 @app.route('/survey/<id>')
 def getSurvey(id):
     data = getResponse(requestURI + '/surveys/' + id)
@@ -58,69 +158,13 @@ def getDataPoint(id):
 
 @app.route('/collections/<survey_id>/<form_id>')
 def getData(survey_id, form_id):
-    collections = []
-    data = getResponse(requestURI + '/surveys/' + survey_id)
-    data['meta_url'] = request.host_url + 'datapoint/' + data['id']
-    # submitter ID
-    submitter_id = data['name'].split('_')[1]
-    acc_detail = None
-    for account in accounts:
-        if account['id'] == submitter_id:
-            acc_detail = account
-    meta = data['forms'][0]['questionGroups'][0]['questions']
-    for form in data['forms']:
-        par_id = form['questionGroups'][0]['id']
-        answers = getResponse(requestURI + '/form_instances?survey_id=' + survey_id + '&form_id=' + form_id)
-        for answer in answers['formInstances']:
-            resp = [
-                {'key':'keymd5', 'value':keymd5},
-                {'key':'ACC_ID', 'value': submitter_id},
-            ]
-            for mt in meta:
-                try:
-                    values = answer['responses'][par_id][0][mt['id']]
-                    if mt['type'] == 'CASCADE':
-                        resp.append({'key':'ID_Commodity', 'value':values[2]['code']})
-                        resp.append({'key':'ID_Agency', 'value':values[1]['code']})
-                    elif mt['type'] == 'DATE':
-                        new_date = datetime.strptime(values[:10],'%Y-%m-%d')
-                        new_date = datetime.strftime(new_date,'%d/%m/%Y')
-                        resp.append({'key':'Date_var', 'value':new_date})
-                    else:
-                        resp.append({'key':mt['variableName'],'value':str(values)})
-                except:
-                    resp.append({'key':mt['variableName'],'value':'0'})
-            url = postURI + '/login_return_ACC_ID'
-            login = payload([
-                {'key':'keymd5', 'value':keymd5},
-                {'key':'acc_name', 'value':acc_detail['name']},
-                {'key':'acc_pass', 'value':acc_detail['pass']}
-            ])
-            u = requests.post(url, data = login, headers = headers)
-            uid = xmltodict.parse(u.text)
-            ids = uid['short']['#text']
-            if ids == '0':
-                log = 'error login'
-            else:
-                url = postURI + '/add_price_return_newid'
-                r = requests.post(url, data = payload(resp), headers = headers)
-                try:
-                    log = xmltodict.parse(r.text)
-                    log = log['string']['#text']
-                except xmltodict.expat.ExpatError:
-                    log = r.text
-            collections.append({
-                'id': answer['id'],
-                'submitter':answer['submitter'],
-                'identifier':answer['identifier'],
-                'device':answer['deviceIdentifier'],
-                'submited_at':answer['submissionDate'],
-                'login':acc_detail,
-                'payload':payload(resp),
-                'response':log
-            })
-    return jsonify(collections)
-
+    lists = []
+    sid = getResponse(requestURI + '/surveys/' + survey_id)
+    responses = getRawData(survey_id, sid['forms'][0]['id'])
+    if responses is not None:
+        for resp in responses:
+            lists.append(resp)
+    return jsonify(lists)
 
 @app.route('/input-list/<survey_id>/<form_id>')
 def inputList(survey_id, form_id):
