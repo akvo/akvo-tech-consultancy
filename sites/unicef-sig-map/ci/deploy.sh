d2032e8803d1c43accbd5a7910762e79c05161e1
Iván Perdomo <ivan@perdomo.me>
2018-11-14 15:53:22 +0100

    Recover rsync call to deploy

--- sites/unicef-sig-map/ci/deploy.sh
+++ sites/unicef-sig-map/ci/deploy.sh
@@ -2,20 +2,17 @@
 
 set -eu
 
-sudo ls -laht public/
-
 # Change owner as docker generated files
 # are usually owned by root
 sudo chown "${USER}:" . -R
 
-sudo ls -laht public/
-
 rm -rf node_modules
 
-# rsync --archive \
-#       --verbose \
-#       --compress \
-#       --progress \
-#       --exclude=ci \
-#       --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-#       . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
+rsync --archive \
+      --quiet \
+      --times \
+      --compress \
+      --progress \
+      --exclude=ci \
+      --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+      . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
