a775672d140f6964e0e76bc5c10cf5fe06cce166
Iván Perdomo <ivan@perdomo.me>
2018-11-14 12:43:43 +0100

    First attempt to automatic deploy

--- sites/unicef-sig-map/ci/deploy.sh
+++ sites/unicef-sig-map/ci/deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 set -eu
+
+rm -rf node_modules
+
+rsync -avz --exclude=ci -e "ssh -i ${SITES_SSH_KEY} -p 18765" --progress . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
