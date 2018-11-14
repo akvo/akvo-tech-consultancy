ceb3a423bfb9e718f754aa5934247512b551386c
Iván Perdomo <ivan@perdomo.me>
2018-11-14 13:53:55 +0100

    List files before/after chown

--- sites/unicef-sig-map/ci/deploy.sh
+++ sites/unicef-sig-map/ci/deploy.sh
@@ -2,21 +2,20 @@
 
 set -eu
 
+sudo ls -laht public/
+
 # Change owner as docker generated files
 # are usually owned by root
-#sudo chown "${USER}:" . -R
-
-# Make sure public folder is not writable by others
-#chmod 755 public
-
-sudo rm -rf node_modules
+sudo chown "${USER}:" . -R
 
 sudo ls -laht public/
 
-rsync --archive \
-      --verbose \
-      --compress \
-      --progress \
-      --exclude=ci \
-      --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-      . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
+rm -rf node_modules
+
+# rsync --archive \
+#       --verbose \
+#       --compress \
+#       --progress \
+#       --exclude=ci \
+#       --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+#       . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
