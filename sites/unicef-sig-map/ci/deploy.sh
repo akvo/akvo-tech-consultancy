9cac57cc9adda667b73b9a73eb6e19a5011a5e2e
Iván Perdomo <ivan@perdomo.me>
2018-11-14 16:03:20 +0100

    Display the version of rsync

--- sites/unicef-sig-map/ci/deploy.sh
+++ sites/unicef-sig-map/ci/deploy.sh
@@ -2,11 +2,15 @@
 
 set -eu
 
+rsync --version
+
 # Change owner as docker generated files
 # are usually owned by root
 sudo chown "${USER}:" . -R
 
 rm -rf node_modules
+
+echo "Deploying using with $(rsync --version | head -n1)"
 
 rsync --archive \
       --quiet \
@@ -16,3 +20,5 @@ rsync --archive \
       --exclude=ci \
       --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
       . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
+
+echo "Done"
