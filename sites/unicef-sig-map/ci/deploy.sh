b0e410ab3cad536ade89a63455e85fd02c675936
Iván Perdomo <ivan@perdomo.me>
2018-11-14 13:01:55 +0100

    Use long option names, disable StrictHostKeyChecking at ssh

--- sites/unicef-sig-map/ci/deploy.sh
+++ sites/unicef-sig-map/ci/deploy.sh
@@ -6,4 +6,10 @@ sudo chown "${USER}:" . -R
 
 rm -rf node_modules
 
-rsync -avz --exclude=ci -e "ssh -i ${SITES_SSH_KEY} -p 18765" --progress . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
+rsync --archive \
+      --verbose \
+      --compress \
+      --progress \
+      --exclude=ci \
+      --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+      . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
