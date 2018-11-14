8df311bdf41201b3e0222c1545b466fafed7c14e
Iván Perdomo <ivan@perdomo.me>
2018-11-14 12:53:31 +0100

    Make sure we change the owner to the default $USER

--- sites/unicef-sig-map/ci/deploy.sh
+++ sites/unicef-sig-map/ci/deploy.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+sudo chown "${USER}:" . -R
+
 rm -rf node_modules
 
 rsync -avz --exclude=ci -e "ssh -i ${SITES_SSH_KEY} -p 18765" --progress . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
