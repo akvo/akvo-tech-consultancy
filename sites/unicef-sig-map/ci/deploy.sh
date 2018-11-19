fb2d8b089bafda1c895e1379f51ed0988c4e0e58
Iván Perdomo <ivan@perdomo.me>
2018-11-19 10:46:49 +0100

    Fix variable used by ssh/rsync

--- sites/unicef-sig-map/ci/deploy.sh
+++ sites/unicef-sig-map/ci/deploy.sh
@@ -17,17 +17,17 @@ rsync \
     --progress \
     --exclude=ci \
     --exclude=node_modules \
-    --rsh="ssh -i ${SSH_KEY_PATH} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+    --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
     . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-sig-map/
 
 # Fix permissions
-ssh -i "${SSH_KEY_PATH}" \
+ssh -i "${SITES_SSH_KEY}" \
     -p 18765 \
     -o UserKnownHostsFile=/dev/null \
     -o StrictHostKeyChecking=no \
     tcakvo@109.73.232.40 'find  ~/public_html/unicef-sig-map/ -type f -print0 | xargs -0 -n1 chmod 644'
 
-ssh -i "${SSH_KEY_PATH}" \
+ssh -i "${SITES_SSH_KEY}" \
     -p 18765 \
     -o UserKnownHostsFile=/dev/null \
     -o StrictHostKeyChecking=no \
