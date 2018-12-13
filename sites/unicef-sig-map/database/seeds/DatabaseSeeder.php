cb72c4b7d7b0215b13d50decbfa6a31012caf288
dedenbangkit <mail@dedenbangkit.com>
2018-12-13 16:01:34 +0800

    add database class

---
+++ sites/unicef-sig-map/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::create([
+            'name' => 'Admin',
+            'email' => '',
+            'password' => bcrypt('')
+        ]);
+    }
+}
