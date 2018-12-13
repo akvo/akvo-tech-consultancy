2242d97f63d7a615451e164e812229cf38805ea1
dedenbangkit <mail@dedenbangkit.com>
2018-12-13 15:46:52 +0800

    auth

--- sites/unicef-sig-map/app/Http/Controllers/Auth/LoginController.php
+++ sites/unicef-sig-map/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use ResponseCache;
 
 class LoginController extends Controller
 {
@@ -34,6 +35,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        ResponseCache::clear();
         $this->middleware('guest')->except('logout');
     }
 }
--- sites/unicef-sig-map/app/Http/Controllers/HomeController.php
+++ sites/unicef-sig-map/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use ResponseCache;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        ResponseCache::clear();
         return view('home');
     }
 }
