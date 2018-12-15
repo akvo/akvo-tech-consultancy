d7fab4dd22c7fa4bec95f32a1ac138265bcf3ecc
Iván Perdomo <ivan@perdomo.me>
2018-12-15 11:04:21 +0100

    Fix build by using an example .env file

--- sites/test-1/ci/build.sh
+++ sites/test-1/ci/build.sh
@@ -2,12 +2,14 @@
 
 set -eu
 
+cp .env.example .env
+
 docker run \
        --rm \
        --volume "$(pwd):/home/tcakvo/public_html/test-1" \
        --workdir "/home/tcakvo/public_html/test-1" \
        --entrypoint /bin/sh \
-       composer:1.8.0 -c 'composer install && php artisan key:generate && php artisan cache:clear'
+       composer:1.8.0 -c 'composer install && php artisan cache:clear'
 
 docker run \
        --rm \
