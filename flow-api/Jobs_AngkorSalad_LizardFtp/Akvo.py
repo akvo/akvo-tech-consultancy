c390e826323028db3f2b77cd1997278437276d79
dedenbangkit <mail@dedenbangkit.com>
2019-02-20 15:48:29 +0800

    First Initialize

---
+++ flow-api/Jobs_AngkorSalad_LizardFtp/Akvo.py
@@ -0,0 +1,38 @@
+import requests as r
+import os, sys
+
+class Flow:
+    tokenURI = 'https://login.akvo.org/auth/realms/akvo/protocol/openid-connect/token'
+    rtData = {
+        'client_id':'curl',
+        'username': 'deden@akvo.org', 
+        'password': 'Jalanremaja1208', 
+        'grant_type':'password',
+        'scope':'openid offline_access'
+    }
+    
+    def refreshData():
+        tokens = r.post(Flow.tokenURI, Flow.rtData).json();
+        return tokens['refresh_token']
+
+    def getAccessToken():
+        account = {
+            'client_id':'curl',
+            'refresh_token': Flow.refreshData(),
+            'grant_type':'refresh_token'
+        }
+        try:
+            account = r.post(Flow.tokenURI, account).json();
+        except:
+            logging.error('FAILED: TOKEN ACCESS UNKNOWN')
+            return False
+        return account['access_token']
+
+    def getResponse(self):
+        header = {
+            'Authorization':'Bearer ' + Flow.getAccessToken(),
+            'Accept': 'application/vnd.akvo.flow.v2+json',
+            'User-Agent':'python-requests/2.14.2'
+        }
+        response = r.get(self, headers=header).json()
+        return response
