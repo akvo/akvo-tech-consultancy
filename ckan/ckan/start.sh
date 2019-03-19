e094dd0562700d6bc97ac5f9c137d97ed98ddad6
Dan Lebrero <dlebrero@gmail.com>
2019-03-19 23:42:12 +0100

    [#3] Fix CKAN config

--- ckan/ckan/start.sh
+++ ckan/ckan/start.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 sql=$(printf '%s\n' "$DB_URL" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+read_db=$(printf '%s\n' "$DATASTORE_READ_DB" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+write_db=$(printf '%s\n' "$DATASTORE_WRITE_DB" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
 
-sed -i /etc/ckan/default/production.ini -e 's/ckan.site_url.*/ckan.site_url = https:\/\/ckan.akvotest.org\//' -e "s/sqlalchemy.url.*/sqlalchemy.url = $sql/" -e '/^ckan.plugins =/ s/$/ datastore datapusher/'
-
-echo "ckan.datastore.read_url = $DATASTORE_READ_DB" >> /etc/ckan/default/production.ini
-echo "ckan.datastore.write_url = $DATASTORE_WRITE_DB" >> /etc/ckan/default/production.ini
-echo "ckan.datapusher.url = http://127.0.0.1:8800/" >> /etc/ckan/default/production.ini
-echo "ckan.storage_path = /var/lib/ckan/storage" >> /etc/ckan/defaul/production.ini
+sed -i /etc/ckan/default/production.ini \
+    -e 's/ckan.site_url.*/ckan.site_url = https:\/\/ckan.akvotest.org\//' \
+    -e "s/sqlalchemy.url.*/sqlalchemy.url = $sql/" \
+    -e '/^ckan.plugins =/ s/$/ datastore datapusher/' \
+    -e "s/#ckan.datastore.read_url.*/ckan.datastore.read_url  = $read_db/" \
+    -e "s/#ckan.datastore.write_url.*/ckan.datastore.write_url = $write_db/" \
+    -e "s/#ckan.storage_path.*/ckan.storage_path = \/var\/lib\/ckan\/storage/" \
+    -e "s/#ckan.datapusher.url.*/ckan.datapusher.url = http:\/\/127.0.0.1:8800\//"
 
 if [[ ! -d "/var/lib/ckan/storage" ]]; then
     mkdir -p /var/lib/ckan/storage
