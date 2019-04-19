4d42239f6836d22d924f14e32520ef8d0fa145e3
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-04-19 16:56:17 +0200

    [#4] Deploy of as-flow-chatbot

--- sites/as-flow-chatbot/run.sh
+++ sites/as-flow-chatbot/run.sh
@@ -1,5 +1,1 @@
-export KEYCLOAK_USER="KEYCLOAK_USER"
-export KEYCLOAK_PWD="KEYCLOAK_PWD"
-export TWILLIO_SID="TWILLIO_SID"
-export TWILLIO_TOKEN="TWILLIO_TOKEN"
-nohup python chatbot.py & echo -e kill $! "\n"rm kill.sh > kill.sh
+python app.py
