8125dce2f0cf48331728f629b736bab6eb55bd29
dedenbangkit <mail@dedenbangkit.com>
2019-04-26 13:33:21 +0800

    Reload Instance #5

--- sites/gc-flow-price-webhook/app.py
+++ sites/gc-flow-price-webhook/app.py
@@ -294,7 +294,6 @@ def update():
     update_thread.run()
     return jsonify({'response':runjob})
 
-
 if __name__ == '__main__':
     app.config.update(DEBUG=True)
     socketio.run(app, host='0.0.0.0', port=3000, debug=True)
