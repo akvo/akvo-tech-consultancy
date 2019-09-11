4e6b5eebfedcc45f70eca97ff5a30d8ebf641d3e
dedenbangkit <mail@dedenbangkit.com>
2019-09-11 18:20:13 +0800

    Change environment

--- sites/appsa-api/app.py
+++ sites/appsa-api/app.py
@@ -47,7 +47,7 @@ def index():
         project = rsr.api('prod', 'project', 'id', p)['results'][0]
         projects.append(project)
     return render_template('index.html',
-            environment = '',
+            environment = '/appsa-api',
             project_parent = project_parent,
             projects = projects,
             countries = countries)
