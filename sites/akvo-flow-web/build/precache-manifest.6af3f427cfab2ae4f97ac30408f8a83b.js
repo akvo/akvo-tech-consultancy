0875728b6769d6448c7fb9ad7502d9ffc99615ac
dedenbangkit <mail@dedenbangkit.com>
2019-10-30 16:39:16 +0800

    Skip Build Part

---
+++ sites/akvo-flow-web/build/precache-manifest.6af3f427cfab2ae4f97ac30408f8a83b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9c6c30842f17134bad3282b8ae3667",
+    "url": "/akvo-flow-web/index.html"
+  },
+  {
+    "revision": "81f1a598c94d59d4ffa5",
+    "url": "/akvo-flow-web/static/css/2.5e3fb3fa.chunk.css"
+  },
+  {
+    "revision": "a00bbca101983e2f9671",
+    "url": "/akvo-flow-web/static/css/main.c9bf2bc9.chunk.css"
+  },
+  {
+    "revision": "81f1a598c94d59d4ffa5",
+    "url": "/akvo-flow-web/static/js/2.e2d3be11.chunk.js"
+  },
+  {
+    "revision": "a00bbca101983e2f9671",
+    "url": "/akvo-flow-web/static/js/main.3d1f96ed.chunk.js"
+  },
+  {
+    "revision": "efd400460111dd5b74b3",
+    "url": "/akvo-flow-web/static/js/runtime~main.7129806f.js"
+  }
+]);
