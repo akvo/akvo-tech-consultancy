5e3a925330033c766e0445077ce49de8e269848d
Rifqi Alfian <merembablas@mrm.local>
2019-11-08 05:30:16 +0700

    Add database migration

---
+++ sites/2scale/database/migrations/2019_11_07_041713_create_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('question_id');
+            $table->char('type', 12);
+            $table->text('text');
+            $table->bigInteger('form_id');
+            $table->bigInteger('survey_id');
+            $table->timestamps();
+
+            $table->index('survey_id');
+            $table->index('form_id');
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
---
+++ sites/2scale/database/migrations/2019_11_07_041722_create_data_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('question_id');
+            $table->bigInteger('datapoint_id');
+            $table->text('answer');
+            $table->char('country', 20);
+            $table->timestamps();
+
+            $table->index('question_id');
+            $table->index('country');
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+}
