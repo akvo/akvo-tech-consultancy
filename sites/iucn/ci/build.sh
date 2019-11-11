fac8fc308e9f7dfce0ef0594fc203e7abf5de652
dedenbangkit <mail@dedenbangkit.com>
2019-11-11 13:43:29 +0800

    Modify Build.sh

--- sites/iucn/ci/build.sh
+++ sites/iucn/ci/build.sh
@@ -7,4 +7,4 @@ cp .env.prod .env
 docker run \
        --rm \
        --volume "$(pwd):/home/tcakvo/public_html/iucn" \
-       --workdir /home/tcakvo/public_html/iucn \
+       --workdir /home/tcakvo/public_html/iucn
