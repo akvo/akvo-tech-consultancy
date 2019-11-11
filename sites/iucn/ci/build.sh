b86f92d2bc79337f1e3eabb4b340ab9b3420c9eb
dedenbangkit <mail@dedenbangkit.com>
2019-11-11 13:42:34 +0800

    add build.sh

---
+++ sites/iucn/ci/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cp .env.prod .env
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/iucn" \
+       --workdir /home/tcakvo/public_html/iucn \
