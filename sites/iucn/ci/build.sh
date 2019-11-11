bfd3a2f4cb59d7fe8e16e4d4c7a5019f32232bfe
dedenbangkit <mail@dedenbangkit.com>
2019-11-11 13:46:18 +0800

    remove build part

--- sites/iucn/ci/build.sh
+++ sites/iucn/ci/build.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
 set -eu
-
-cp .env.prod .env
-
-docker run \
-       --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/iucn" \
-       --workdir /home/tcakvo/public_html/iucn
