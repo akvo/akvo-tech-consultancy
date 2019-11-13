226ef5b901f08a0e42f33184f4a5d279b17c1674
dedenbangkit <mail@dedenbangkit.com>
2019-11-13 18:04:28 +0800

    New cache

--- lumen-api/lumen-echarts-generator/figures/Akvo.py
+++ lumen-api/lumen-echarts-generator/figures/Akvo.py
@@ -4,17 +4,17 @@ import os
 class Flow:
     def getToken():
         authentification = {
-            'client_id':'curl',
             'username': os.environ['AUTH0_USER'],
             'password': os.environ['AUTH0_PWD'],
             'grant_type':'password',
             'audience': 'https://akvofoundation.eu.auth0.com/api/v2/',
             'scope': 'openid profile email',
-	    'client_id': 'KOgRM2Uam6FXOZdwKs3AKU7I8VtGKsiu',
-	    'client_secret': os.environ['AUTH0_CLIENT_PASSWORD'],
+            'client_id': 'KOgRM2Uam6FXOZdwKs3AKU7I8VtGKsiu',
+            'client_secret': os.environ['AUTH0_CLIENT_PASSWORD'],
         }
         tokenURI = 'https://akvofoundation.eu.auth0.com/oauth/token'
         tokens = r.post(tokenURI, authentification).json()
+        print(token)
         return tokens['id_token']
 
     def getData(self, accessToken):
