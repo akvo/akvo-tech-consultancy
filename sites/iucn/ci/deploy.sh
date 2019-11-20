c30d70faf18ca97cc44aed72efe27a443dbe0bcc
Rifqi Alfian <merembablas@mrm.local>
2019-11-20 12:34:16 +0700

    Update iucn deploy file to copy .htaccess #48

--- sites/iucn/ci/deploy.sh
+++ sites/iucn/ci/deploy.sh
@@ -33,4 +33,22 @@ ssh -i "${SITES_SSH_KEY}" \
     -o StrictHostKeyChecking=no \
     tcakvo@109.73.232.40 'find ~/public_html/iucn/ -type d -print0 | xargs -0 -n1 chmod 755'
 
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'cp ~/etc/tc.akvo.org/iucn.tof.htaccess ~/public_html/iucn/trees-on-farms/.htaccess'
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'cp ~/etc/tc.akvo.org/iucn.plus.htaccess ~/public_html/iucn/plus/.htaccess'
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'cp ~/etc/tc.akvo.org/iucn.plastics.htaccess ~/public_html/iucn/plastics/.htaccess'
+
 echo "Done"
