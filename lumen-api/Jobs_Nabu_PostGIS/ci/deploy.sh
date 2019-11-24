5e9121f33f90e80c83c3c7e3baebc26fdd0bf5a0
Iván Perdomo <ivan@perdomo.me>
2019-11-24 11:39:34 +0100

    [#55] Deploy Docker image

---
+++ lumen-api/Jobs_Nabu_PostGIS/ci/deploy.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function log {
+   echo "$(date +"%T") - INFO - $*"
+}
+
+export PROJECT_NAME=akvo-lumen
+
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    exit 0
+fi
+
+log Making sure gcloud and kubectl are installed and up to date
+gcloud components install kubectl
+gcloud components update
+gcloud version
+command -v gcloud kubectl
+
+log Authentication with gcloud and kubectl
+gcloud auth activate-service-account --key-file "${GCLOUD_ACCOUNT_FILE}" --project "${PROJECT_NAME}"
+gcloud config set container/cluster europe-west1-d
+gcloud config set compute/zone europe-west1-d
+gcloud config set container/use_client_certificate True
+
+## TODO!!! Change to prod! Decide if we publish to test or not
+if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+    log Environment is production
+    gcloud container clusters get-credentials test
+else
+    log Environement is test
+    gcloud container clusters get-credentials test
+fi
+
+log Pushing images
+gcloud auth configure-docker
+docker push eu.gcr.io/${PROJECT_NAME}/pricing-integration-greencoffee
+
+#sed -e "s/\${TRAVIS_COMMIT}/$TRAVIS_COMMIT/" ci/k8s/cronjob.yaml.template > cronjob.yaml.donotcommit
+
+#kubectl apply -f cronjob.yaml.donotcommit
