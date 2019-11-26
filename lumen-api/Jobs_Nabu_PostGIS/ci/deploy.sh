8a097990f4f466cc3f951c1dd4f535ad63e75b56
Iván Perdomo <ivan@perdomo.me>
2019-11-26 09:41:01 +0100

    [#55] Recover check on `develop` and `master` branch

--- lumen-api/Jobs_Nabu_PostGIS/ci/deploy.sh
+++ lumen-api/Jobs_Nabu_PostGIS/ci/deploy.sh
@@ -8,9 +8,9 @@ function log {
 
 export PROJECT_NAME=akvo-lumen
 
-#if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
-#    exit 0
-#fi
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
 
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     exit 0
