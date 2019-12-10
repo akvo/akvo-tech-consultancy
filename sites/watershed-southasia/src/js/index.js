b6ab320fb57242889f4a33db363dfa01f2c17c30
Rifqi Alfian <merembablas@mrm.local>
2019-12-10 14:48:25 +0700

    Update view #64

--- sites/watershed-southasia/src/js/index.js
+++ sites/watershed-southasia/src/js/index.js
@@ -2,9 +2,160 @@ import '../css/app.css';
 
 const $ = require('jquery');
 const echarts = require('echarts');
+const L = require('leaflet');
 const data = require('../json/data.json');
 const indicator = require('../json/indicator.json');
 
 
 let combined = [...data, ...indicator];
 console.log(combined);
+var xing = '';
+
+var fig71a = echarts.init(document.getElementById('fig71a'));
+            
+// specify chart configuration item and data
+var option = {
+    title: {
+        text: 'ECharts entry example'
+    },
+    tooltip: {},
+    legend: {
+        data:['Sales']
+    },
+    xAxis: {
+        data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+    },
+    yAxis: {},
+    series: [{
+        name: 'Sales',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+};
+
+// use configuration item and data specified to show chart
+fig71a.setOption(option);
+
+
+var fig71d = echarts.init(document.getElementById('fig71d'));
+            
+// specify chart configuration item and data
+var option = {
+    title: {
+        text: 'ECharts entry example'
+    },
+    tooltip: {},
+    legend: {
+        data:['Sales']
+    },
+    xAxis: {
+        data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+    },
+    yAxis: {},
+    series: [{
+        name: 'Sales',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+};
+
+// use configuration item and data specified to show chart
+fig71d.setOption(option);
+
+
+var fig71c = echarts.init(document.getElementById('fig71c'));
+
+var option = {
+    title : {
+        text: '某站点用户访问来源',
+        subtext: '纯属虚构',
+        x:'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+    },
+    series : [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '60%'],
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1548, name:'搜索引擎'}
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+};
+
+fig71c.setOption(option);
+
+
+var fig71e = echarts.init(document.getElementById('fig71e'));
+
+var option = {
+    title : {
+        text: '某站点用户访问来源',
+        subtext: '纯属虚构',
+        x:'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+    },
+    series : [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '60%'],
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1548, name:'搜索引擎'}
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+};
+
+fig71e.setOption(option);
+
+var mymap = L.map('map', { maxZoom: 10 }).setView([0, 0], 0);
+
+var tileServer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var tileAttribution = 'Tiles © Wikimedia — Source: OpenStreetMap, Data: Unicef Pacific WASH, <a href="https://akvo.org">Akvo SEAP</a>';
+
+L.tileLayer(tileServer, {
+    attribution: tileAttribution,
+    maxZoom: 18
+}).addTo(mymap);
