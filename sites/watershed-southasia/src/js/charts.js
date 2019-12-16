a7fba04f1031a33f8f52df888311e437de5e7520
Rifqi Alfian <merembablas@gmail.com>
2019-12-16 15:16:38 +0700

    Update generate chart use index instead key value #64

--- sites/watershed-southasia/src/js/charts.js
+++ sites/watershed-southasia/src/js/charts.js
@@ -1,49 +1,7 @@
 import { titleCase } from './util.js';
-import { getOptions } from './data.js';
 const echarts = require('echarts');
 const L = require('leaflet');
 const _ = require('lodash');
-
-export const getCharts = (title, row, info, md, color) => {
-    let id = title.split(" ")[0];
-    let html = `<div class="col-md-` + md + `">
-                <div class="card">
-                  <div class="card-header gradient-card-header ` + color + `-gradient">` + titleCase(title) + `</div>
-                  <div class="card-body">
-                    <div id="` + id + `" style="height:450px"></div>
-                  </div>
-                  <div class="card-footer text-muted">` + info.content + `</div>
-                </div>
-                </div>`;
-    $("#" + row).append(html);
-    var element = document.getElementById(id);
-    var myChart = echarts.init(element);
-    var option = getOptions(info.type, id);
-    myChart.setOption(option);
-    return myChart;
-}
-
-export const getMaps = (title, row, info, md, color) => {
-    let id = title.split(" ")[0];
-    let html = `<div class="col-md-` + md + `">
-                <div class="card">
-                  <div class="card-header gradient-card-header ` + color + `-gradient">` + titleCase(title) + `</div>
-                  <div class="card-body">
-                    <div id="` + id + `" style="height:450px"></div>
-                  </div>
-                  <div class="card-footer text-muted">` + info.content + `</div>
-                </div>
-                </div>`;
-    $("#" + row).append(html);
-    var option = getOptions(info.type, id);
-    const mymap = L.map(id, { maxZoom: option.maxZoom }).setView([option.lat, option.lng], 15);
-    mymap.scrollWheelZoom.disable();
-    const tileServer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    const tileAttribution = 'Tiles © Wikimedia — Source: OpenStreetMap, Data: Akvolumen';
-    L.tileLayer(tileServer, {
-        attribution: tileAttribution,
-    }).addTo(mymap);
-}
 
 export const initChart = (figure, data) => {
   let id = figure['title'].split(" ")[0];
@@ -61,12 +19,14 @@ export const initChart = (figure, data) => {
   var myChart = echarts.init(element);
   myChart['figure'] = figure;
   
-  let legend = _(data).map(x => x[figure['legend']] ).uniq().value();
-  let xAxis = _(data).map(x => x[figure['x']] ).uniq().value();
-  let sums = _(data).countBy(figure['x']).value();
+  let indexLegend = data.names.indexOf(figure['legend']);
+  let indexX = data.names.indexOf(figure['x']);
+  let legend = _(data.values).map(x => x[indexLegend] ).uniq().value();
+  let xAxis = _(data.values).map(x => x[indexX] ).uniq().value();
+  let sums = _(data.values).countBy(indexX).value();
 
-  let series = _(data).groupBy(figure['legend']).map((items, val) => {
-      let values = _(items).countBy(figure['x']).value();
+  let series = _(data.values).groupBy(indexLegend).map((items, val) => {
+      let values = _(items).countBy(indexX).value();
       let dataSeries = _(xAxis).map(x => {
           let defVal = values[x] == undefined ? null : values[x];
           if (defVal === null || defVal === 0) {
@@ -110,12 +70,14 @@ export const initChart = (figure, data) => {
 
 export const updateChart = (chart, data) => {
   let figure = chart['figure'];
-  let legend = _(data).map(x => x[figure['legend']] ).uniq().value();
-  let xAxis = _(data).map(x => x[figure['x']] ).uniq().value();
-  let sums = _(data).countBy(figure['x']).value();
+  let indexLegend = data.names.indexOf(figure['legend']);
+  let indexX = data.names.indexOf(figure['x']);
+  let legend = _(data.values).map(x => x[indexLegend] ).uniq().value();
+  let xAxis = _(data.values).map(x => x[indexX] ).uniq().value();
+  let sums = _(data.values).countBy(indexX).value();
 
-  let series = _(data).groupBy(figure['legend']).map((items, val) => {
-      let values = _(items).countBy(figure['x']).value();
+  let series = _(data.values).groupBy(indexLegend).map((items, val) => {
+      let values = _(items).countBy(indexX).value();
       let dataSeries = _(xAxis).map(x => {
           let defVal = values[x] == undefined ? 0 : values[x];
           return Math.round((defVal * 100) / sums[x]);
--- sites/watershed-southasia/src/js/index.js
+++ sites/watershed-southasia/src/js/index.js
@@ -7,14 +7,17 @@ const filters = require('../json/filters.json');
 const figures = require('../json/figures.json');
 import { initChart, updateChart, getMaps } from './charts.js';
 
-let newData = [];
+let newData = {};
 let chartFigs = [];
+let filterIndex1 = data.names.indexOf('village-habitation_Gram_Panchayat');
+let filterIndex2 = data.names.indexOf('village-habitation_Village');
 
 function updateData() {
     let filter = {};
-    $('#dr-loc-gram-panchayat').html() != 'Location Gram Panchayat' ? (filter['T'] = $('#dr-loc-gram-panchayat').html()) : null;
-    $('#dr-loc-village').html() != 'Location Village' ? (filter['U'] = $('#dr-loc-village').html()) : null;
-    newData = _.filter(data.values, filter);
+    $('#dr-loc-gram-panchayat').html() != 'Location Gram Panchayat' ? (filter[filterIndex1] = $('#dr-loc-gram-panchayat').html()) : null;
+    $('#dr-loc-village').html() != 'Location Village' ? (filter[filterIndex2] = $('#dr-loc-village').html()) : null;
+    newData['names'] = data.names;
+    newData['values'] = _.filter(data.values, filter);
     _.forEach(chartFigs, (chart) => {
         updateChart(chart, newData);
     });
