1b4cef1558ab966b8ebfff394ab79b3c81a796e8
dedenbangkit <mail@dedenbangkit.com>
2019-12-17 23:30:44 +0700

    Change build volumes

--- sites/watershed-southasia/ci/build.sh
+++ sites/watershed-southasia/ci/build.sh
@@ -4,7 +4,7 @@ set -eu
 
 docker run \
        --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/watershedchart/indiabaseline" \
+       --volume "$(pwd):/home/tcakvo/public_html/watershedchart" \
        --workdir "/home/tcakvo/public_html/watershedchart/indiabaseline" \
        --entrypoint /bin/sh \
        node:8-alpine -c 'npm i && npm run build'
