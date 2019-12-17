86b0f19fa7fe5a6eba9f7dac0f9c8ab8e11ebba7
Rifqi Alfian <merembablas@gmail.com>
2019-12-17 22:19:24 +0700

    Fix build process #64

--- sites/watershed-southasia/ci/build.sh
+++ sites/watershed-southasia/ci/build.sh
@@ -2,9 +2,3 @@
 
 set -eu
 
-docker run \
-       --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/watershedchart/indiabaseline" \
-       --workdir "/home/tcakvo/public_html/watershedchart/indiabaseline" \
-       --entrypoint /bin/sh \
-       node:8-alpine -c 'npm i && npm run build'
