7b84c0cf13dd14a7ffa1e1514a772a2e8caeafc6
dedenbangkit <mail@dedenbangkit.com>
2019-12-17 13:57:07 +0700

    Simplify Container #64

--- sites/watershed-southasia/src/js/charts.js
+++ sites/watershed-southasia/src/js/charts.js
@@ -1,16 +1,22 @@
-import { insert, titleCase } from './util.js';
+import {
+    insert,
+    titleCase
+} from './util.js';
+import {
+    newContainer
+} from './util.js';
 const echarts = require('echarts');
 const L = require('leaflet');
 const _ = require('lodash');
 
-
 export const initChart = (figure, data) => {
-  let id = figure['title'].split(" ")[0].replace(/\./g,'');
-  let html = `<div class="col-md-` + figure['width'] + `">
+    newContainer(figure.position);
+    let id = figure['title'].split(" ")[0].replace(/\./g, '');
+    let html = `<div class="col-md-` + figure['width'] + `">
               <div class="card">
                 <div class="card-header gradient-card-header">` + titleCase(figure['title']) + `</div>
                 <div class="card-body">
-                    <div class="loading-frame" id="loading-`+ id +`" >
+                    <div class="loading-frame" id="loading-` + id + `" >
                     <div class="spinner-border text-primary">
                     </div>
                     </div>
@@ -19,12 +25,102 @@ export const initChart = (figure, data) => {
                 <div class="card-footer text-muted">` + figure['content'] + `</div>
               </div>
               </div>`;
-  $("#" + figure['position']).append(html);
-  var element = document.getElementById(id);
-  var myChart = echarts.init(element);
-  myChart['figure'] = figure;
+    $("#" + figure['position']).append(html);
+    var element = document.getElementById(id);
+    var myChart = echarts.init(element);
+    myChart['figure'] = figure;
 
-  if (figure['type'] == 'bar') {
+    if (figure['type'] == 'bar') {
+        let indexLegend = data.names.indexOf(figure['legend']);
+        let indexX = data.names.indexOf(figure['x']);
+        let legend = _(data.values).map(x => x[indexLegend]).uniq().value();
+        let xAxis = _(data.values).map(x => x[indexX]).uniq().value();
+        let sums = _(data.values).countBy(indexX).value();
+
+        let series = _(data.values).groupBy(indexLegend).map((items, val) => {
+            let values = _(items).countBy(indexX).value();
+            let dataSeries = _(xAxis).map(x => {
+                let defVal = values[x] == undefined ? null : values[x];
+                if (defVal === null || defVal === 0) {
+                    return null;
+                }
+                return ((defVal * 100) / sums[x]).toFixed(0);
+            }).value();
+
+            return {
+                name: val,
+                type: 'bar',
+                stack: "total",
+                zLevel: 3,
+                label: {
+                    normal: {
+                        fontSize: 14,
+                        show: true,
+                        position: "inside",
+                        formatter: "{c}%"
+                    }
+                },
+                data: dataSeries
+            }
+        }).value();
+
+        myChart.setOption({
+            tooltip: {},
+            legend: {
+                data: legend
+            },
+            xAxis: {
+                data: _.map(xAxis, x => insert(x, 10, "\n")),
+                axisLabel: {
+                    rotate: 305
+                }
+            },
+            yAxis: {},
+            series
+        });
+        $("#loading-" + id).remove();
+        return myChart;
+    }
+
+    if (figure['type'] == 'pie') {
+        let indexLegend = data.names.indexOf(figure['legend']);
+        let legend = _(data.values).map(x => x[indexLegend]).uniq().value();
+        let chartData = _(data.values).countBy(indexLegend).value();
+        let series = _(legend).map(x => {
+            return {
+                value: chartData[x],
+                name: x
+            };
+        }).value();
+
+        myChart.setOption({
+            tooltip: {},
+            legend: {
+                data: legend
+            },
+            series: {
+                type: 'pie',
+                radius: ['70%', '30%'],
+                center: ['50%', '50%'],
+                zLevel: 3,
+                label: {
+                    normal: {
+                        fontSize: 14,
+                        show: true,
+                        position: "inside",
+                        formatter: "{d}%\n({c})"
+                    }
+                },
+                data: series
+            }
+        });
+        $("#loading-" + id).remove();
+        return myChart;
+    }
+}
+
+export const updateChart = (chart, data) => {
+    let figure = chart['figure'];
     let indexLegend = data.names.indexOf(figure['legend']);
     let indexX = data.names.indexOf(figure['x']);
     let legend = _(data.values).map(x => x[indexLegend]).uniq().value();
@@ -38,29 +134,31 @@ export const initChart = (figure, data) => {
             if (defVal === null || defVal === 0) {
                 return null;
             }
-            return ((defVal * 100) / sums[x]).toFixed(0);
+            return Math.round((defVal * 100) / sums[x]);
         }).value();
 
         return {
-          name: val,
-          type: 'bar',
-          stack: "total",
-          zLevel: 3,
-          label: {
-              normal: {
-                  fontSize: 14,
-                  show: true,
-                  position: "inside",
-                  formatter: "{c}%"
-              }
-          },
-          data: dataSeries
-      }  
+            name: val,
+            type: 'bar',
+            stack: "total",
+            zLevel: 3,
+            label: {
+                normal: {
+                    fontSize: 14,
+                    show: true,
+                    position: "inside",
+                    formatter: "{c}%"
+                }
+            },
+            data: dataSeries
+        }
     }).value();
 
-    myChart.setOption({
+    chart.setOption({
         tooltip: {},
-        legend: { data: legend },
+        legend: {
+            data: legend
+        },
         xAxis: {
             data: _.map(xAxis, x => insert(x, 10, "\n")),
             axisLabel: {
@@ -70,90 +168,4 @@ export const initChart = (figure, data) => {
         yAxis: {},
         series
     });
-    $("#loading-" + id).remove();
-    return myChart;
-  }
-  
-  if (figure['type'] == 'pie') {
-    let indexLegend = data.names.indexOf(figure['legend']);
-    let legend = _(data.values).map(x => x[indexLegend]).uniq().value();
-    let chartData = _(data.values).countBy(indexLegend).value();
-    let series = _(legend).map(x => {
-      return {
-        value: chartData[x],
-        name: x
-      };
-    }).value();
-    
-    myChart.setOption({
-        tooltip: {},
-        legend: { data: legend },
-        series: {
-          type: 'pie',
-          radius: ['70%', '30%'],
-          center: ['50%', '50%'],
-          zLevel: 3,
-          label: {
-              normal: {
-                  fontSize: 14,
-                  show: true,
-                  position: "inside",
-                  formatter: "{d}%\n({c})"
-              }
-          },
-          data: series
-        }
-    });
-    $("#loading-" + id).remove();
-    return myChart;
-  }
-}
-
-export const updateChart = (chart, data) => {
-  let figure = chart['figure'];
-  let indexLegend = data.names.indexOf(figure['legend']);
-  let indexX = data.names.indexOf(figure['x']);
-  let legend = _(data.values).map(x => x[indexLegend]).uniq().value();
-  let xAxis = _(data.values).map(x => x[indexX]).uniq().value();
-  let sums = _(data.values).countBy(indexX).value();
-
-  let series = _(data.values).groupBy(indexLegend).map((items, val) => {
-      let values = _(items).countBy(indexX).value();
-      let dataSeries = _(xAxis).map(x => {
-      let defVal = values[x] == undefined ? null : values[x];
-      if (defVal === null || defVal === 0) {
-          return null;
-      }
-        return Math.round((defVal * 100) / sums[x]);
-      }).value();
-
-      return {
-          name: val,
-          type: 'bar',
-          stack: "total",
-          zLevel: 3,
-          label: {
-              normal: {
-                  fontSize: 14,
-                  show: true,
-                  position: "inside",
-                  formatter: "{c}%"
-              }
-          },
-          data: dataSeries
-      }
-  }).value();
-
-  chart.setOption({
-      tooltip: {},
-      legend: { data: legend },
-      xAxis: {
-          data: _.map(xAxis, x => insert(x, 10, "\n")),
-          axisLabel: {
-              rotate: 305
-          }
-      },
-      yAxis: {},
-      series
-  });
 }
