1af09dfa3c88fe01927455d70a589df878ac0886
Iván Perdomo <ivan@perdomo.me>
2020-02-07 13:36:56 +0100

    [#74] Run migrate.sh instead of run.sh

--- flow-api/Jobs_Uduma_ReportUpdate/migrate.sh
+++ flow-api/Jobs_Uduma_ReportUpdate/migrate.sh
@@ -1,4 +1,8 @@
-python migrations/manage.py version_control postgresql://$PSQL_USER:$PSQL_PWD@$PSQL_HOST/uduma_report migrations
-migrate manage manage.py --repository=migrations --url=postgresql://$PSQL_USER:$PSQL_PWD@$PSQL_HOST/uduma_report
+#!/usr/bin/env bash
+
+set -eu
+
+python migrations/manage.py version_control "postgresql://$PSQL_USER:$PSQL_PWD@$PSQL_HOST/uduma_report migrations"
+migrate manage manage.py --repository=migrations --url="postgresql://$PSQL_USER:$PSQL_PWD@$PSQL_HOST/uduma_report"
 python manage.py upgrade
 python init.py
