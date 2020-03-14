8626d7b9fb998dab88a377a057a9ca1b97215c6a
Rifqi Alfian <merembablas@gmail.com>
2020-03-14 21:29:47 +0700

    SpiceUp Plot Notification add latest update from Jupyter

--- flow-api/Jobs_SpiceUp_PlotsNotification/app2.py
+++ flow-api/Jobs_SpiceUp_PlotsNotification/app2.py
@@ -14,19 +14,22 @@ from email import encoders
 from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 today_date = datetime.today().date()
 week_ago = today_date - timedelta(days=15)
-start_date = (today_date - timedelta(days=15)).strftime('%d-%m-%Y')
-end_date = today_date.strftime('%d-%m-%Y')
+start_date_raw = (today_date - timedelta(days=15))
+start_date = (today_date - timedelta(days=15)).strftime('%Y-%m-%d')
+end_date = today_date.strftime('%Y-%m-%d')
 day = today_date.strftime('%d')
 
 if (day == '01' or day == '15'):
 
     instanceURI = 'spiceup'
     requestURI = 'https://api.akvo.org/flow/orgs/' + instanceURI
-    EMAIL_RECEPIENTS = ['hatami@cinquer.co.id','indri@cinquer.nl','yayang@cinquer.nl','d.kurniawati@icco.nl','everschuren@verstegen.nl','aharton2002@yahoo.com','otihrostiana@gmail.com','dyah_manohara@yahoo.com','joy@akvo.org','ima@akvo.org','deden@akvo.org','merembablas@gmail.com']
-    #EMAIL_RECEPIENTS = ['merembablas@gmail.com']
+    #EMAIL_RECEPIENTS = ['hatami@cinquer.co.id','indri@cinquer.nl','yayang@cinquer.nl','d.kurniawati@icco.nl','everschuren@verstegen.nl','aharton2002@yahoo.com','otihrostiana@gmail.com','dyah_manohara@yahoo.com','joy@akvo.org','ima@akvo.org','deden@akvo.org','merembablas@gmail.com']
+    EMAIL_RECEPIENTS = ['merembablas@gmail.com']
     EMAIL_SENDER = os.environ['EMAIL_USER']
     EMAIL_PASSWORD = os.environ['EMAIL_PWD']
 
@@ -116,26 +119,19 @@ if (day == '01' or day == '15'):
                 pass
     allResponses = newResponses
 
-    this_month = (today_date - timedelta(days=30)).strftime('%B')
-
     email_data = {}
-
 
     plant = allResponses['01. Plot Registration']['Plant Registration'][0]
     plot_location = plant['Plot Location'].split('|')
     plot_location = [pl.split(':')[0] for pl in plot_location]
     plot_location = ', '.join(plot_location)
-    plot_location
-
     email_data.update({'plot_location':plot_location})
 
     plot_latlong = [str(plant['Plot Geopoint_lat']),str(plant['Plot Geopoint_long'])]
     plot_latlong = ', '.join(plot_latlong)
     plot_map_img = "https://www.mapquestapi.com/staticmap/v4/getmap?size=640,480&type=map&pois=mcenter," + plot_latlong
     plot_map_img = plot_map_img +"|&zoom=12&center="+ plot_latlong +"&imagetype=JPEG&traffic=flow&scalebar=false&key=lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24"
-
     plot_link = "http://maps.google.com/maps?t=k&q=loc:" + plot_latlong
-
     email_data.update({'plot_link':plot_link})
 
     plant_submissionDate = plant['submissionDate'].split('T')[0]
@@ -143,21 +139,22 @@ if (day == '01' or day == '15'):
     plot_submission_summary =  plot_submission_summary + datetime.strptime(plant_submissionDate, '%Y-%m-%d').strftime('%B, %d')
     plant_type = plant['Type of Plantation']
     plot_submission_summary = plot_submission_summary + ' with ' + plant_type + ' in ' + plot_location + '.'
-
     email_data.update({'plot_submission_summary':plot_submission_summary})
 
     weather = pd.DataFrame(allResponses['02. Weather Conditions']['Weather Conditions'])
     weather['General Weather Conditions'] = weather['General Weather Conditions'].apply(lambda x: x.replace(':',''))
     weather['createdAt'] = weather['createdAt'].astype('datetime64[ns]')
     weather['month'] = weather['createdAt'].apply(lambda x : x.strftime('%B'))
-    mask = (weather['createdAt'] > start_date) & (weather['createdAt'] <= end_date)
+    mask = (weather['createdAt'] > start_date)
     weather = weather.loc[mask]
 
     try:
         weather_average_temperature = list(weather.groupby('identifier').mean().reset_index()['Temperature (Celsius)'])[0]
         weather_average_temperature = str(int(weather_average_temperature)) + '° Celcious'
     except:
-        weather_average_temperature = 'nan'
+        weather_average_temperature = 'No Data'
+
+    weather_submission = len(weather.index)
 
     email_data.update({'weather_average_temperature':weather_average_temperature})
 
@@ -167,7 +164,7 @@ if (day == '01' or day == '15'):
     try:
         weather_most_condition = 'Mostly ' + weather_most_condition
     except:
-        weather_most_condition = 'nan'
+        weather_most_condition = 'No Data'
 
     email_data.update({'weather_most_condition':weather_most_condition})
 
@@ -177,72 +174,118 @@ if (day == '01' or day == '15'):
 
     soil = pd.DataFrame(allResponses['03. Soil Monitoring']['Soil Properties'])
     soil['createdAt'] = soil['createdAt'].astype('datetime64[ns]')
-    mask = (soil['createdAt'] > start_date) & (soil['createdAt'] <= end_date)
+    mask = (soil['createdAt'] > start_date)
     soil = soil.loc[mask]
+    soil_submission = len(soil.index)
 
     def get_soil_result(x):
         if x is not None:
             return float(x['result'][0]['value'])
         else:
             return 0
-
+    
     soil['Soil Moisture'] = soil['Soil Moisture'].apply(get_soil_result)
     soil['Soil pH'] = soil['Soil pH'].apply(get_soil_result)
     soil['Nitrate'] = soil['Nitrate'].apply(get_soil_result)
     soil['P'] = soil['P'].apply(get_soil_result)
 
-    soil_average = soil.groupby('identifier').mean().reset_index()
-    soil_p = round(soil_average['P'], 2)
-    soil_ph = round(soil_average['Soil pH'], 2)
-    soil_nitrate = round(soil_average['Nitrate'][0], 2)
-    soil_moisture = round(soil_average['Soil Moisture'][0], 2)
-    soil_df = (1 + (soil_moisture * 1.3)) * 5
-    soil_formula = round((soil_nitrate + 0.613) * soil_df / 2.7763, 2)
+    soil_average = {}
+    try:
+        soil_average['Soil Moisture'] = round(soil.loc[soil['Soil Moisture'] != 0].groupby('identifier').mean().reset_index()['Soil Moisture'][0], 2)
+    except:
+        soil_average['Soil Moisture'] = 'No Data'
 
-    planthealth = pd.DataFrame(allResponses['04. Plant Monitoring'])
+    try:
+        soil_average['Soil pH'] = round(soil.loc[soil['Soil pH'] != 0].groupby('identifier').mean().reset_index()['Soil pH'][0], 2)
+    except:
+        soil_average['Soil pH'] = 'No Data'
+        
+    try:
+        soil_average['Nitrate'] = round(soil.loc[soil['Nitrate'] != 0].groupby('identifier').mean().reset_index()['Nitrate'][0], 2)
+    except:
+        soil_average['Nitrate'] = 'No Data'
+        
+    try:
+        soil_average['P'] = round(soil.loc[soil['P'] != 0].groupby('identifier').mean().reset_index()['P'][0], 2)
+    except:
+        soil_average['P'] = 'No Data'
+
+    # SOIL FORMULA
+    if soil_average['Soil Moisture'] == 'No Data' or soil_average['Nitrate'] == 'No Data':
+        soil_df = 0
+        soil_formula = 0
+    else:
+        soil_df = (1 + (soil_average['Soil Moisture'] * 1.3)) * 5
+        soil_formula = round((soil_average['Nitrate'] + 0.613) * soil_df / 2.7763, 2)
+
+    # PLANT GENERAL CONDITION
     plant_gc = pd.DataFrame(allResponses['04. Plant Monitoring']['General Condition'])
     plant_gc['createdAt'] = plant_gc['createdAt'].astype('datetime64[ns]')
-    mask = (plant_gc['createdAt'] > start_date) & (plant_gc['createdAt'] <= end_date)
+    mask = (plant_gc['createdAt'] > start_date)
     plant_gc = plant_gc.loc[mask]
+    plant_gc_submission = len(plant_gc.index)
 
-    plant_height_max = plant_gc.max()
-    plant_height_min = plant_gc.min()
-    plant_height_avg = plant_gc.mean()
+    variables = ['Plant Condition', 'Plant / Vine Condition','Roots / Stem Base']
+    conditions = []
+    for item in variables:
+        tmp_arr = plant_gc[[item, 'Plant ID']].groupby(item).count().reset_index().to_dict('records')
+        result = map(lambda x: str(x['Plant ID']) + ' ' + x[item], tmp_arr)
+        result = []
+        for item2 in tmp_arr:
+            if item2[item] == 'Dead':
+                result.append('<span style="color:red;font-weight:bold">' + str(item2['Plant ID']) + ' ' + item2[item] + '</span>')
+            else:
+                result.append(str(item2['Plant ID']) + ' ' + item2[item])
+                
+        conditions.append('- ' + item + ': ' + ', '.join(result))
 
-    variables = ['Plant / Vine Condition','Roots / Stem Base']
-    conditions = plant_gc[variables + ['Plant ID']].groupby(variables).count().reset_index().to_dict('records')
-    plant_general_condition = []
-    for condition in conditions:
-        plant_general_condition.append(str(condition['Plant ID']) + " plants exhibited " + condition['Roots / Stem Base'].lower() + " roots and " + condition['Plant / Vine Condition'].lower() + " vine condition")
+    plant_general_condition = "<br/> ".join(conditions)
 
-    plant_general_condition = ', '.join(plant_general_condition)
-
+    # PLANT NODE
     plant_growth = pd.DataFrame(allResponses['04. Plant Monitoring']['Plant Growth'])
     plant_growth['createdAt'] = plant_growth['createdAt'].astype('datetime64[ns]')
-    mask = (plant_growth['createdAt'] > start_date) & (plant_growth['createdAt'] <= end_date)
+    mask = (plant_growth['createdAt'] > start_date)
     plant_growth = plant_growth.loc[mask]
+    plant_growth_submission = len(plant_growth.index)
+    plant_node = plant_growth[plant_growth['Number of Nodes'].notnull()]['Number of Nodes'].describe()
+    plant_node = plant_node.round(1).fillna('No Data')
 
-    plant_node = plant_growth['Number of Nodes'].describe()
-    plant_leaf = plant_growth['Number of Leaves'].describe()
-    plant_branch = plant_growth['Number of Branches'].describe()
-    plant_height = plant_growth['Plant Height (cm)'].describe()
-    plant_orthotropic = plant_growth['Number of Orthotropic shoots'].describe()
+    # PLANT LEAF
+    plant_leaf = plant_growth[plant_growth['Number of Leaves'].notnull()]['Number of Leaves'].describe()
+    plant_leaf = plant_leaf.round(1).fillna('No Data')
 
+    # PLANT BRANCH
+    plant_branch = plant_growth[plant_growth['Number of Branches'].notnull()]['Number of Branches'].describe()
+    plant_branch = plant_branch.round(1).fillna('No Data')
+
+    plant_height = plant_growth[plant_growth['Plant Height (cm)'].notnull()]['Plant Height (cm)'].describe()
+    plant_height = plant_height.round(1).fillna('No Data')
+
+    plant_orthotropic = plant_growth[plant_growth['Number of Orthotropic shoots'].notnull()]['Number of Orthotropic shoots'].describe()
+    plant_orthotropic = plant_orthotropic.round(1).fillna('No Data')
+
+    # OTHER INFORMATION
     plant_oi = pd.DataFrame(allResponses['04. Plant Monitoring']['Other Information'])
     plant_oi['createdAt'] = plant_oi['createdAt'].astype('datetime64[ns]')
-    mask = (plant_oi['createdAt'] > start_date) & (plant_oi['createdAt'] <= end_date)
+    mask = (plant_oi['createdAt'] > start_date)
     plant_oi = plant_oi.loc[mask]
     plant_oi_max = plant_oi.max()
     plant_oi_min = plant_oi.min()
     plant_oi_avg = plant_oi.mean()
+
     plant_oi_hanging = plant_oi.groupby('Leaves hanging rigidly').size()
     plant_oi_wilting = plant_oi.groupby('Wilting Symptoms').size()
 
+    # ACTIVITIES
     activities = pd.DataFrame.from_dict(allResponses['05. Activity']['Activity Monitoring'])
     activities['createdAt'] = activities['createdAt'].astype('datetime64[ns]')
-    mask = (activities['createdAt'] > start_date) & (activities['createdAt'] <= end_date)
+    mask = (activities['createdAt'] > start_date)
     activities = activities.loc[mask]
     type_of_activities = activities['Type of Activity'].unique()
+    if activities['Type of Activity'].count() == 0:
+        type_of_activities=['No Activities']
+    
+    activities_submission = len(activities.index)
 
     weather_img_mapping = {
         'Mostly Cloudy': 'https://i.imgur.com/RP7sg1j.png',
@@ -255,11 +298,12 @@ if (day == '01' or day == '15'):
     }
 
     today = date.today()
-
+    
     file_loader = FileSystemLoader('.')
     env = Environment(loader=file_loader)
     template = env.get_template('./email_v2.html')
     html_output = template.render(
+        start_date = start_date_raw.strftime("%B %d, %Y"),
         date = today.strftime("%B %d, %Y"),
         plot_summary = plot_submission_summary,
         img_map = 'https://www.mapquestapi.com/staticmap/v4/getmap?size=640,480&amp;type=map&amp;pois=mcenter,-2.3428743096598823, 106.18896435946226|&amp;zoom=12&amp;center=-2.3428743096598823, 106.18896435946226&amp;imagetype=JPEG&amp;traffic=flow&amp;scalebar=false&amp;key=lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24',
@@ -267,10 +311,10 @@ if (day == '01' or day == '15'):
         weather_icon = weather_img_mapping.get(weather_most_condition, weather_img_mapping['Mostly Sunny']),
         weather_label = weather_most_condition,
         weather_temp = weather_average_temperature,
-        soil_moisture = round(soil_average['Soil Moisture'][0], 2),
-        soil_ph = round(soil_average['Soil pH'][0], 2),
-        soil_nitrate = round(soil_average['Nitrate'][0], 2),
-        soil_p = round(soil_average['P'][0], 2),
+        soil_moisture = soil_average['Soil Moisture'],
+        soil_ph = soil_average['Soil pH'],
+        soil_nitrate = soil_average['Nitrate'],
+        soil_p = soil_average['P'],
         plant_general_condition = plant_general_condition,
         plant_leaf = plant_leaf,
         plant_node = plant_node,
@@ -279,9 +323,12 @@ if (day == '01' or day == '15'):
         plant_oi_hanging = plant_oi_hanging,
         plant_oi_wilting = plant_oi_wilting,
         plant_orthotropic = plant_orthotropic,
-        type_of_activities = type_of_activities
+        type_of_activities = type_of_activities,
+        weather_submission = weather_submission,
+        soil_submission = soil_submission,
+        plant_growth_submission = plant_growth_submission,
+        activities_submission = activities_submission
     )
-
 
     fout = "./email_v2.html"
 
