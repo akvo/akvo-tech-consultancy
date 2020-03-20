40e9f9c9baac6957edb346d15f0e743b22406c6c
Rifqi Alfian <merembablas@gmail.com>
2020-03-20 10:16:24 +0700

    Nabu email change cron duration

--- lumen-api/Jobs_Nabu_EmailData/app.py
+++ lumen-api/Jobs_Nabu_EmailData/app.py
@@ -25,11 +25,9 @@ POSTGRES_PORT = os.environ['POSTGRES_PORT']
 POSTGRES_USER = os.environ['POSTGRES_USER']
 POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
 
-#EMAIL_RECEIVER = ['Stefanie.Brandes@NABU.de', 'Svane.Bender@NABU.de', 'Mesfin.Tekle.nabu@gmail.com']
-EMAIL_RECEIVER = ['merembablas@gmail.com']
+EMAIL_RECEIVER = ['Stefanie.Brandes@NABU.de', 'Svane.Bender@NABU.de', 'Mesfin.Tekle.nabu@gmail.com']
 EMAIL_SENDER = 'akvo.tech.consultancy@gmail.com'
-#EMAIL_BCC = ['joy@akvo.org', 'deden@akvo.org']
-EMAIL_BCC = ['mrmrfqlfn@gmail.com', 'akvo.tech.consultancy@gmail.com']
+EMAIL_BCC = ['joy@akvo.org', 'deden@akvo.org']
 
 today = date.today()
 today_str = today.strftime("%d/%m/%Y")
