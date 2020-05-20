e045b01c5dc6ed9375f8cfb0e96efd41c0589860
dedenbangkit <mail@dedenbangkit.com>
2020-05-20 10:41:25 +0700

    [#121] Add Deployment CI

---
+++ sites/unicef-kenya-5w/ci/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+#cp .env.prod .env
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unicef-kenya-5w" \
+       --workdir /home/tcakvo/public_html/unicef-kenya-5w\
+       --entrypoint /bin/sh \
+       composer -c 'composer install'
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unicef-kenya-5w" \
+       --workdir /home/tcakvo/public_html/unicef-kenya-5w\
+       --entrypoint /bin/sh \
+       composer -c 'composer dump-autoload'
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unicef-kenya-5w" \
+       --workdir "/home/tcakvo/public_html/unicef-kenya-5w" \
+       --entrypoint /bin/sh \
+       node:8-alpine -c 'npm i && npm run prod'
---
+++ sites/unicef-kenya-5w/ci/deploy.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -eu
+
+rsync --version
+
+# Change owner as docker generated files
+# are usually owned by root
+sudo chown "${USER}:" . -R
+
+echo "Deploying site..."
+
+rsync \
+    --archive \
+    --compress \
+    --progress \
+    --exclude=ci \
+    --exclude=node_modules \
+    --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+    . tcakvo@109.73.232.40:/home/tcakvo/public_html/unicef-kenya-5w/
+
+echo "Fixing permissions..."
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'find ~/public_html/unicef-kenya-5w/ -not -path "*.well-known*" -type f -print0 | xargs -0 -n1 chmod 644'
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'find ~/public_html/unicef-kenya-5w/ -type d -print0 | xargs -0 -n1 chmod 755'
+
+echo "Copy the config..."
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'cp ~/etc/tc.akvo.org/unicef-kenya-5w.env.prod ~/public_html/unicef-kenya-5w/.env'
+
+echo "Clearing cache..."
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'cd ~/public_html/unicef-kenya-5w/ && /usr/local/bin/php72 artisan cache:clear'
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 'cd ~/public_html/unicef-kenya-5w/ && /usr/local/bin/composer dump-autoload'
+
+echo "Done"
