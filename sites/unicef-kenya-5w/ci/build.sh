b54ad13aecc0a22bb535393c7d75b37d4facf88e
dedenbangkit <mail@dedenbangkit.com>
2020-05-21 10:44:02 +0700

    [#121] Add mix.env

--- sites/unicef-kenya-5w/ci/build.sh
+++ sites/unicef-kenya-5w/ci/build.sh
@@ -24,4 +24,5 @@ docker run \
        --volume "$(pwd):/home/tcakvo/public_html/unicef-kenya-5w" \
        --workdir /home/tcakvo/public_html/unicef-kenya-5w\
        --entrypoint /bin/sh \
+       --env MIX_PUBLIC_URL="https://covid-ke5w.tc.akvo.org" \
        node:8-alpine -c 'npm i && npm run prod'
