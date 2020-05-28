b21fbc08bf21936073194a9ab35f4280649ba1d4
dedenbangkit <mail@dedenbangkit.com>
2020-05-28 09:11:49 +0700

    [#73] Add new project

--- sites/portfolio/prod.sh
+++ sites/portfolio/prod.sh
@@ -1,6 +1,6 @@
-echo PUBLIC_URL=$PUBLIC_URL > .env
-echo REACT_APP_STATIC_USER=$REACT_APP_STATIC_USER >> .env
-echo REACT_APP_STATIC_PWD=$REACT_APP_STATIC_PWD >> .env
+echo PUBLIC_URL=$PORTFOLIO_PUBLIC_URL > .env
+echo REACT_APP_STATIC_USER=$PORTFOLIO_REACT_APP_STATIC_USER >> .env
+echo REACT_APP_STATIC_PWD=$PORTFOLIO_REACT_APP_STATIC_PWD >> .env
 
 yarn build
 rsync --archive --compress --progress ./build/ siteground:/home/tcakvo/public_html/portfolio
