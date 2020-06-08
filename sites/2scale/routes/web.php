c2cdb7c0a1f5647cb3b8c005a3b44e005d8ea5cd
wgprtm <wgprtm@gmail.com>
2020-06-08 09:10:17 +0800

    Create route group

--- sites/2scale/routes/web.php
+++ sites/2scale/routes/web.php
@@ -5,6 +5,10 @@
 |--------------------------------------------------------------------------
 */
 
+Route::middleware(['auth'])->group(function() {
+    Route::get( '/logout', 'Auth\Auth0IndexController@logout' )->name('logout');
+});
+
 Route::get('/', 'PageController@home')->name('home');
 Route::get('/dashboard', 'PageController@dashboard')->name('dashboard');
 Route::get('/database', 'PageController@database')->name('database');
@@ -15,7 +19,6 @@ Route::get('/reach-and-react', 'PageController@reachreact')->name('reachreact');
 
 Route::get( '/auth0/callback', '\Auth0\Login\Auth0Controller@callback' )->name('auth0-callback');
 Route::get( '/login', 'Auth\Auth0IndexController@login' )->name('login');
-Route::get( '/logout', 'Auth\Auth0IndexController@logout' )->name('logout')->middleware('auth');
 /*
 |--------------------------------------------------------------------------
 | Frames
