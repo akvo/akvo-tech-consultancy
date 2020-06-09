3933631ada6de6dfbdfc2696c367c96f79acc078
dedenbangkit <mail@dedenbangkit.com>
2020-06-09 18:14:49 +0700

    [#134] Migrate Database

--- sites/akvo-flow-web-api/run.sh
+++ sites/akvo-flow-web-api/run.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+connection="$(python test-connection.py)"
+
+if [[ "${connection}" != "OK" ]]; then
+    echo "Unable to connect to PostgreSQL"
+    exit 1
+fi
+
+flask db upgrade
 python app.py
