553ef33766e5127fbbabf5ed7c92423b6d589836
wayangalihpratama <wgprtm@gmail.com>
2020-06-11 12:08:35 +0800

    [#141] Change flow environment

--- sites/2scale/config/akvo-auth0.php
+++ sites/2scale/config/akvo-auth0.php
@@ -1,13 +1,11 @@
 <?php
 
 $INSTANCE = env('AKVO_INSTANCE', '');
-$BASE_AUTH_URL = 'https://akvo.eu.auth0.com/oauth/token';
-$API_FLOW = 'https://api-auth0.akvotest.org/flow/orgs/'.$INSTANCE.'/folders?parentId=0';
+$API_FLOW = env('FLOW_API_URL', '').'/flow/orgs/'.$INSTANCE.'/folders?parentId=0';
 
 return [
     'instance' => $INSTANCE,
     'endpoints' => [
-        'login' => $BASE_AUTH_URL,
         'fetch_flow' => $API_FLOW,
     ],
 ];
