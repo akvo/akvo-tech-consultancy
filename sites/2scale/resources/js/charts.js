6dce7678d6892ba94d1ae04ea5f7c167e8a705e3
dedenbangkit <mail@dedenbangkit.com>
2020-06-15 18:52:44 +0700

    [#156] filter function for organisation

--- sites/2scale/resources/js/charts.js
+++ sites/2scale/resources/js/charts.js
@@ -132,3 +132,102 @@ export const getMaps = () => {
         });
     ;
 }
+
+export const getHierarchy = (data) => {
+    var element = document.getElementById('hierarchy');
+    var myChart = echarts.init(element);
+    echarts.util.each(data.children, function(datum, index) {
+        index % 2 === 0 && (datum.collapsed = true);
+    });
+    var option = {
+        tooltip: {
+            trigger: 'item',
+            triggerOn: 'mousemove'
+        },
+        series: [{
+            room: 'zoom',
+            zoom: true,
+            type: 'tree',
+
+            data: [data],
+            initialTreeDepth: 2,
+            top: '1%',
+            left: '7%',
+            bottom: '1%',
+            right: '20%',
+            symbolSize: 8,
+            itemStyle: {
+                borderWidth:0,
+            },
+            lineStyle: {
+                curveness: .3,
+            },
+            symbolSize: function(params, name) {
+                if (name.data.value === "organisations") {
+                    return 5;
+                };
+                if (name.data.value === "projects") {
+                    return 7;
+                };
+                if (name.data.value === "partnership") {
+                    return 10;
+                };
+                if (name.data.value === "countries") {
+                    return 15;
+                };
+                return 12;
+            },
+            symbol: function(params, name) {
+                if (name.data.value === "organisations") {
+                    return 'rect';
+                };
+                if (name.data.value === "projects") {
+                    return 'circle';
+                };
+                if (name.data.value === "partnership") {
+                    return 'triangle';
+                };
+                if (name.data.value === "countries") {
+                    return 'roundRect';
+                };
+                return 'diamond';
+            },
+            emphasis: {
+                label: {
+                    backgroundColor: '#000',
+                    color: '#fff',
+                    padding: 5,
+                },
+                lineStyle: {
+                    width: 2,
+                },
+                itemStyle: {
+                    color: '#ff1744'
+                }
+            },
+            label: {
+                normal: {
+                    position: 'left',
+                    verticalAlign: 'middle',
+                    align: 'right',
+                    fontSize: 12,
+                    fontFamily: 'Roboto'
+                }
+            },
+            leaves: {
+                label: {
+                    normal: {
+                        position: 'right',
+                        verticalAlign: 'middle',
+                        align: 'left'
+                    }
+                }
+            },
+            expandAndCollapse: true,
+            animationDuration: 250,
+            animationEasing: 'backOut',
+            animationDurationUpdate: 250
+        }]
+    };
+    myChart.setOption(option);
+}
