1a44a520118645dbe12dada954d5af30b3573528
dedenbangkit <mail@dedenbangkit.com>
2020-06-18 17:34:22 +0700

    [#149] Restartr build

--- sites/general-scripts/ci/build.sh
+++ sites/general-scripts/ci/build.sh
@@ -2,5 +2,3 @@
 
 set -eu
 
-cat analytics.js | sed s/#pos#/left/g > analytics-left.js
-cat analytics.js | sed s/#pos#/right/g > analytics-right.js
--- sites/general-scripts/ci/deploy.sh
+++ sites/general-scripts/ci/deploy.sh
@@ -12,5 +12,8 @@ fi
 
 echo "Deploying analytics..."
 
+cat analytics.js | sed s/#pos#/left/g > analytics-left.js
+cat analytics.js | sed s/#pos#/right/g > analytics-right.js
+
 scp -i ${SITES_SSH_KEY} -P 18765 analytics-left.js tcakvo@109.73.232.40:/home/tcakvo/public_html/
 scp -i ${SITES_SSH_KEY} -P 18765 analytics-right.js tcakvo@109.73.232.40:/home/tcakvo/public_html/
