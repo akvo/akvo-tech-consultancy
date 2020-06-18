e151832e3620a3520d656caa4c6871bf71078078
dedenbangkit <mail@dedenbangkit.com>
2020-06-18 17:47:16 +0700

    [#149] Redeploy analytics

--- sites/general-scripts/ci/deploy.sh
+++ sites/general-scripts/ci/deploy.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
+rsync --version
+
+# Change owner as docker generated files
+# are usually owned by root
+sudo chown "${USER}:" . -R
+
 if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
     exit 0
 fi
@@ -22,6 +28,12 @@ rsync \
     --exclude=node_modules \
     --rsh="ssh -i ${SITES_SSH_KEY} -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
     . tcakvo@109.73.232.40:/home/tcakvo/public_html/$FOLDER/
+
+ssh -i "${SITES_SSH_KEY}" \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    tcakvo@109.73.232.40 "find ~/public_html/${FOLDER}/ -type d -print0 | xargs -0 -n1 chmod 755"
 
 echo "Duplicate analytics..."
 
