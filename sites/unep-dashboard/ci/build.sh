da50f562409f2036805da9af9a63990b4ee1f005
dedenbangkit <mail@dedenbangkit.com>
2020-07-16 10:13:41 +0700

    [#201] Unep-Dashboard Deployments

---
+++ sites/unep-dashboard/ci/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+#cp .env.prod .env
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
+       --workdir /home/tcakvo/public_html/unep-dashboard \
+       --entrypoint /bin/sh \
+       composer -c 'composer install'
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
+       --workdir /home/tcakvo/public_html/unep-dashboard \
+       --entrypoint /bin/sh \
+       composer -c 'composer dump-autoload'
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
+       --workdir "/home/tcakvo/public_html/unep-dashboard" \
+       --entrypoint /bin/sh \
+       node:8-alpine -c 'npm i && npm run prod'
