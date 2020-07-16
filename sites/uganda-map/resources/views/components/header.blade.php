f0dfc61caa9e2063c91a6ccd10d86934bc884d62
wayangalihpratama <wgprtm@gmail.com>
2020-07-16 13:20:47 +0800

    [#190, #201] Change db json type to text and redeploy

--- sites/uganda-map/resources/views/components/header.blade.php
+++ sites/uganda-map/resources/views/components/header.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="author" content="Akvo">
-    <title>SIG-POC</title>
+    <title>{{ env('APP_NAME') }}</title>
     <link rel="icon" href="{{asset('images/solomon-island-ico.ico')}}">
     <link href="{{asset(mix('css/app.css'))}}" rel="stylesheet">
     <link href="{{asset('vendor/leaflet/leaflet.css')}}" rel="stylesheet">
