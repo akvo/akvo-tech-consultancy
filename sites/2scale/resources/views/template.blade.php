2e8a9576a1621b731df6a177317348d9b6151896
dedenbangkit <mail@dedenbangkit.com>
2020-08-14 17:51:16 +0700

    [#217] Adjust cache version

--- sites/2scale/resources/views/template.blade.php
+++ sites/2scale/resources/views/template.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="cache-version" value="1.0.0">
+    <meta name="cache-version" value="1.1.0">
     <meta name="description" content="2SCALE website">
     <meta name="author" content="Akvo">
     <meta name="keywords" content="2SCALE">
