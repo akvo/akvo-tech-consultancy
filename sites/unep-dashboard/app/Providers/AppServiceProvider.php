44386b164c629aca0ea10288b792273f0da3ff04
dedenbangkit <mail@dedenbangkit.com>
2020-08-18 17:07:40 +0700

    [#216] Change chache version

--- sites/unep-dashboard/app/Providers/AppServiceProvider.php
+++ sites/unep-dashboard/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Blade::directive('money', function ($amount) {
             return "<?php echo 'USD ' . number_format($amount, 2); ?>";
         });
