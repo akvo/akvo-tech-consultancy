c14da4a47e557454c9965bce9206063c65418425
dedenbangkit <mail@dedenbangkit.com>
2020-08-25 10:35:18 +0700

    [#216] Add built by Akvo

--- sites/unep-dashboard/resources/views/index.blade.php
+++ sites/unep-dashboard/resources/views/index.blade.php
@@ -28,5 +28,8 @@
         </div>
         <img src="{{asset('/images/pattern.jpg')}}" width="100px" id="map-pattern" style="display:none;"/>
     </body>
-    <script src="js/app.js" type="text/javascript"></script>
+    <script src="{{mix('/js/app.js')}}" type="text/javascript"></script>
+    <script type="text/javascript" src="https://tc.akvo.org/analytics/analytics-left.js"></script>
+    <noscript><iframe src="//analytics.akvo.org/containers/f6fdd448-a2bc-4734-8aa0-dba4d0d0f2a3/noscript.html" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    </noscript>
 </html>
