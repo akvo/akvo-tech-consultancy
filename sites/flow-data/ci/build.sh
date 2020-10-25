2d4815f0a6ea2327acc54e7b0f78d698188ad1aa
dedenbangkit <mail@dedenbangkit.com>
2020-10-25 17:44:05 +0700

    [#269] remove docker run build

--- sites/flow-data/ci/build.sh
+++ sites/flow-data/ci/build.sh
@@ -3,17 +3,3 @@
 set -eu
 
 #cp .env.prod .env
-
-docker run \
-       --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/flow-data" \
-       --workdir /home/tcakvo/public_html/flow-data \
-       --entrypoint /bin/sh \
-       composer -c 'composer install'
-
-docker run \
-       --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/flow-data " \
-       --workdir /home/tcakvo/public_html/flow-data \
-       --entrypoint /bin/sh \
-       composer -c 'composer dump-autoload'
