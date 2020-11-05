f8e4c6a0664f951316260603b048a91a8f2b7796
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2020-11-05 22:07:27 +0700

    [#268] webform data setup

--- tests/test-akvo-flow-form/form/test_form.py
+++ tests/test-akvo-flow-form/form/test_form.py
@@ -1,74 +1,61 @@
 import xmltodict
 import json
 import pytest
-import requests as r
 
 
-@pytest.fixture()
-def data_setup():
-    import requests as r
-    webform = r.get(
+@pytest.fixture(scope="class")
+def webform_setup(request):
+    import requests
+
+    return requests.get(
         'https://tech-consultancy.akvo.org/akvo-flow-web-api/seap/356020954/update')
 
 
 class TestForm:
     formId = str(356020954)
 
-    def test_webform_connection(self):
-        webform = r.get(
-            'https://tech-consultancy.akvo.org/akvo-flow-web-api/seap/356020954/update')
-        assert webform.status_code == 200
+    def test_webform_connection(self, webform_setup):
+        assert webform_setup.status_code == 200
 
-    def test_element_exists(self):
+    def test_element_exists(self, webform_setup):
         with open('data/356020954.xml') as fd:
             xmlData = xmltodict.parse(fd.read())
-
-        webform = r.get(
-            'https://tech-consultancy.akvo.org/akvo-flow-web-api/seap/356020954/update')
-        jsonData = webform.json()
+        jsonData = webform_setup.json()
 
         for element in xmlData['survey']:
             index = element.replace('@', '')
         assert index in jsonData
 
-    def test_value_exists(self):
+    def test_value_exists(self, webform_setup):
         with open('data/356020954.xml') as fd:
             xmlData = xmltodict.parse(fd.read())
 
-        webform = r.get(
-            'https://tech-consultancy.akvo.org/akvo-flow-web-api/seap/356020954/update')
-        jsonData = webform.json()
+        jsonData = webform_setup.json()
 
         for element in xmlData['survey']:
             if xmlData['survey'][element] is not None:
                 assert jsonData[element.replace('@', '')] is not None
 
-    def test_json_data_type_match(self):
+    def test_json_data_type_match(self, webform_setup):
         with open('data/356020954.xml') as fd:
             xmlData = xmltodict.parse(fd.read())
 
-        webform = r.get(
-            'https://tech-consultancy.akvo.org/akvo-flow-web-api/seap/356020954/update')
-        jsonData = webform.json()
+        jsonData = webform_setup.json()
 
         for element in xmlData['survey']:
             assert isinstance(jsonData[element.replace(
                 '@', '')], type(xmlData['survey'][element]))
 
-    def test_translation_exists(self):
+    def test_translation_exists(self, webform_setup):
         with open('data/356020954.xml') as fd:
             xmlData = xmltodict.parse(fd.read())
 
-        webform = r.get(
-            'https://tech-consultancy.akvo.org/akvo-flow-web-api/seap/356020954/update')
-        jsonData = webform.json()
+        jsonData = webform_setup.json()
         assert 1
 
-    def test_list_with_single_element(self):
+    def test_list_with_single_element(self, webform_setup):
         with open('data/356020954.xml') as fd:
             xmlData = xmltodict.parse(fd.read())
 
-        webform = r.get(
-            'https://tech-consultancy.akvo.org/akvo-flow-web-api/seap/356020954/update')
-        jsonData = webform.json()
+        jsonData = webform_setup.json()
         assert 1
