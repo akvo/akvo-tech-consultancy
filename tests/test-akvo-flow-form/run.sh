1a3f2fad19116a0ce49b38a3d414f66a9a4048fd
dedenbangkit <mail@dedenbangkit.com>
2020-11-09 23:50:50 +0700

    [#268] fixing k8s permission

--- tests/test-akvo-flow-form/run.sh
+++ tests/test-akvo-flow-form/run.sh
@@ -2,13 +2,4 @@
 
 set -euo pipefail
 
-for n in form cascade
-do
-    echo ""
-    echo "** Running test in folder ${n}"
-    echo ""
-    cd ./${n} && pytest -o log_cli=true
-    cd -
-done
-
 python app.py
---
+++ tests/test-akvo-flow-form/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+for n in form cascade
+do
+    echo ""
+    echo "** Running test in folder ${n}"
+    echo ""
+    cd ./${n} && pytest -o log_cli=true
+    cd -
+done
