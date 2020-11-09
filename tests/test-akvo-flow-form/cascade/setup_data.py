2e865aa35021196af3353ee6d7a68b0f42095fcf
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2020-11-09 19:03:18 +0700

    [#268] basic test

---
+++ tests/test-akvo-flow-form/cascade/setup_data.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+class Setup:
+    def __init__(self, cascade_id):
+        self.cascade_id = str(cascade_id)
+        self.db_connection = sqlite3.connect(
+            'data/cascade-'+self.cascade_id+'-v1.sqlite')
+
+    def db_exec(self, query):
+        cursor = self.db_connection.cursor()
+        cursor.execute(query)
+        return cursor
+
+    def fetch_api(self, parent_id):
+        import requests
+        return requests.get('https://tech-consultancy.akvo.org/akvo-flow-web-api/cascade/seap/cascade-6275403559731200-v1.sqlite/0')
