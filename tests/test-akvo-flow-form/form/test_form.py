201195e0f6816b5db8a42e5953c3d6fa157e78d6
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2020-11-17 12:52:40 +0700

    [#268] add tests : question group, question help, validation rules, question attributes

--- tests/test-akvo-flow-form/form/test_form.py
+++ tests/test-akvo-flow-form/form/test_form.py
@@ -1,6 +1,7 @@
 from setup_data import Setup
 
 import pytest
+from distutils import util
 
 
 @pytest.fixture(scope="class")
@@ -15,12 +16,12 @@ class TestForm:
     def test_webform_connection(self, setup):
         assert setup.webform.status_code == 200
 
-    def test_element_exists(self, setup):
+    def test_survey_attribute_exists(self, setup):
         xmlData = setup.xmldata["survey"]
         jsonData = setup.webform.json()
 
-        for element in xmlData:
-            assert element in jsonData
+        for attribute in xmlData:
+            assert attribute in jsonData
 
     def test_value_exists(self, setup):
         xmlData = setup.xmldata["survey"]
@@ -29,6 +30,52 @@ class TestForm:
         for element in xmlData:
             if xmlData[element] is not None:
                 assert jsonData[element] is not None
+
+    def test_number_of_question_groups_should_match(self, setup):
+        xmlData = setup.xmldata["survey"]
+        jsonData = setup.webform.json()
+
+        assert len(xmlData['questionGroup']) == len(jsonData['questionGroup'])
+
+    def test_number_of_question_in_groups_should_match(self, setup):
+        xmlData = setup.xmldata["survey"]
+        jsonData = setup.webform.json()
+
+        for i in range(len(xmlData['questionGroup'])):
+            assert len(xmlData['questionGroup'][i]['question']) == len(
+                jsonData['questionGroup'][i]['question'])
+
+    def test_question_help_should_match(self, setup):
+        xmlData = setup.xmldata["survey"]
+        jsonData = setup.webform.json()
+
+        for i in range(len(xmlData['questionGroup'])):
+            assert len(xmlData['questionGroup'][i]['question']) == len(
+                jsonData['questionGroup'][i]['question'])
+
+    def test_question_help_alt_text_should_match(self, setup):
+        xmlData = setup.xmldata["survey"]
+        jsonData = setup.webform.json()
+
+        for i in range(len(xmlData['questionGroup'])):
+            assert len(xmlData['questionGroup'][i]['question']) == len(
+                jsonData['questionGroup'][i]['question'])
+
+    def test_question_validation_rule_should_match(self, setup):
+        xmlData = setup.xmldata["survey"]
+        jsonData = setup.webform.json()
+
+        for i in range(len(xmlData['questionGroup'])):
+            assert len(xmlData['questionGroup'][i]['question']) == len(
+                jsonData['questionGroup'][i]['question'])
+
+    def test_repeatable_question_groups_should_match(self, setup):
+        xmlData = setup.xmldata["survey"]
+        jsonData = setup.webform.json()
+
+        for i in range(len(xmlData['questionGroup'])):
+            assert bool(util.strtobool(
+                xmlData['questionGroup'][i]['repeatable'])) == jsonData['questionGroup'][i]['repeatable']
 
     def test_json_data_type_match(self, setup):
         xmlData = setup.xmldata["survey"]
