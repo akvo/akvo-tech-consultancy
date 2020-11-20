3a6488fceb1c69e7c6545a4304997db44752cd93
Loïc Sans <loic.sans@gmail.com>
2020-11-20 15:25:18 +0200

    Small touches

--- sites/beyond-chocolate/resources/js/components/WelcomeBanner.js
+++ sites/beyond-chocolate/resources/js/components/WelcomeBanner.js
@@ -5,6 +5,8 @@ const WelcomeBanner = function() {
     return (
         <Jumbotron className="hero">
             <h1>Welcome to Beyond Chocolate Dataportal</h1>
+            <div className="colourBg1"></div>
+            <div className="colourBg2"></div>
         </Jumbotron>
     );
 };
