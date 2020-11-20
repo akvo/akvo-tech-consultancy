e1b9458e79b1c91ec2f6bde3ae5ce56a2e9c9a84
dedenbangkit <mail@dedenbangkit.com>
2020-11-20 12:58:09 +0700

    [#94] Add travis build

--- sites/akvo-flow-web-api/ci/deploy.sh
+++ sites/akvo-flow-web-api/ci/deploy.sh
@@ -16,12 +16,18 @@ if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     exit 0
 fi
 
+log Making sure gcloud and kubectl are installed and up to date
+gcloud components install kubectl
+gcloud components update
+gcloud version
+which gcloud kubectl
+
 log Authentication with gcloud and kubectl
-gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.json
+gcloud auth activate-service-account --key-file "${GCLOUD_ACCOUNT_FILE}"
 gcloud config set project akvo-lumen
 gcloud config set container/cluster europe-west1-d
 gcloud config set compute/zone europe-west1-d
-gcloud config set container/use_client_certificate False
+gcloud config set container/use_client_certificate True
 
 if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
     log Environment is production
