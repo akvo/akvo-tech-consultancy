0e0a048ae24040c48fccfab7f927baf0e41f495a
dedenbangkit <mail@dedenbangkit.com>
2020-11-27 05:11:28 +0700

    [#314] Allow IDH to have save functionality

--- sites/akvo-flow-web/src/util/Environment.js
+++ sites/akvo-flow-web/src/util/Environment.js
@@ -5,6 +5,6 @@ export const API_URL = process.env.NODE_ENV !== "development"
 export const CACHE_URL = window.location.href.split('/').splice(-1)[0].split('-').length === 1 ? false : true;
 export const PARENT_URL = window.location !== window.parent.location;
 export const USING_PASSWORDS = "2scale";
-export const SAVE_FEATURES = ["2scale","seap"];
+export const SAVE_FEATURES = ["2scale","seap","idh"];
 export const CAPTCHA_KEY = process.env.REACT_APP_CAPTCHA_KEY;
 export const READ_CACHE = process.env.NODE_ENV === "development" ? "fetch" : "update";
