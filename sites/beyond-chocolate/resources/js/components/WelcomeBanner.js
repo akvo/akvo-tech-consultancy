b45599c6a97a452844743000b46d20e33ec3d7f6
zuhdil <zuhdil@gmail.com>
2020-11-27 19:42:39 +0700

    [#297] adjust logo position and change welcome banner text

--- sites/beyond-chocolate/resources/js/components/WelcomeBanner.js
+++ sites/beyond-chocolate/resources/js/components/WelcomeBanner.js
@@ -4,7 +4,7 @@ import { Jumbotron } from "react-bootstrap";
 const WelcomeBanner = () => {
     return (
         <Jumbotron className="hero">
-            <h1>Welcome to Beyond Chocolate Dataportal</h1>
+            <h1>Welcome to the GISCO Monitoring Pilot for 2019 data</h1>
             <div className="colourBg1"></div>
             <div className="colourBg2"></div>
         </Jumbotron>
