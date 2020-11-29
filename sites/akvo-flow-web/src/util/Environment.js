e22e612999fb1b4e28a89f24d5f87aea8551e8ef
dedenbangkit <mail@dedenbangkit.com>
2020-11-29 23:03:57 +0700

    [#322] Conditional parameter to extend meta data in save functionality

--- sites/akvo-flow-web/src/util/Environment.js
+++ sites/akvo-flow-web/src/util/Environment.js
@@ -5,6 +5,20 @@ export const API_URL = process.env.NODE_ENV !== "development"
 export const CACHE_URL = window.location.href.split('/').splice(-1)[0].split('-').length === 1 ? false : true;
 export const PARENT_URL = window.location !== window.parent.location;
 export const USING_PASSWORDS = "2scale";
-export const SAVE_FEATURES = ["2scale","seap","idh"];
+export const SAVE_FEATURES = [
+    {
+        instance:"2scale",
+        api: false
+    },
+    {
+        instance:"seap",
+        api: "gisco-demo.tc.akvo.org/api"
+    },
+    {
+        instance:"idh",
+        api: "gisco-demo.tc.akvo.org/api"
+    },
+];
+
 export const CAPTCHA_KEY = process.env.REACT_APP_CAPTCHA_KEY;
 export const READ_CACHE = process.env.NODE_ENV === "development" ? "fetch" : "update";
