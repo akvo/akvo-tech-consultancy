5ee7a4f0fd43f57b2e00fa6538025817ddadd80c
wayangalihpratama <wgprtm@gmail.com>
2020-11-30 16:18:28 +0800

    [#321] Use helper to send email

--- sites/beyond-chocolate/app/Http/Controllers/EmailController.php
+++ sites/beyond-chocolate/app/Http/Controllers/EmailController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \Mailjet\Resources;
 use \Mailjet\LaravelMailjet\Facades\Mailjet;
+use App\Helpers\Mails;
+
+/**
+ * Need to change this email into new mail helpers
+ */
 
 class EmailController extends Controller
 {
-    public function send(Request $request)
+    public function send(Request $request, Mails $mails)
     {
         $recipients = config('mail.mailing.list');
         $recipients = collect($recipients)->map(function($address){
@@ -16,27 +21,41 @@ class EmailController extends Controller
                 'Email' => $address
             ];
         })->push(['Email' => $request->email]);
-        $mailjet = Mailjet::getClient();
-        $body = [
-            'FromEmail' => config('mail.mailing.host'),
-            'FromName' => config('mail.mailing.host'),
-            'Subject' => $request->subject,
-            'Html-part' => "Feedback From: <strong>$request->email</strong><br/><br/>
+        $subject = $request->subject;
+        $body = "Feedback from: <strong>$request->email</strong><br/><br/>
                 $request->message <hr/>
-                <strong>SENT VIA
-                <a href='".env('APP_URL')."'>Cacao - Beyond Chocolate</a></strong>
-                <br/>",
-            'Recipients' => $recipients
-        ];
+                <strong>SENT VIA <a href='".env('APP_URL')."'>Cacao - Beyond Chocolate</a></strong>
+                <br/>";
+        $text = "Feedback from: $request->email";
+        $response = $mails->sendEmail($recipients, false, $subject, $body, $text);
+        
+        return response([
+            'message' => 'Your feedback has been sent!', 'mails' => $response
+        ]);
 
-        $response = false;
-        try {
-            $response =  $mailjet->post(Resources::$Email, ['body' => $body]);
-            $response = $response->success();
-        } catch (\Exception $e) {
-            logger()->error('Goutte client error ' . $e->getMessage());
-        }
-        return ['sent' => $response];
+        // old way
+        // $mailjet = Mailjet::getClient();
+        // $body = [
+        //     'FromEmail' => config('mail.mailing.host'),
+        //     'FromName' => config('mail.mailing.host'),
+        //     'Subject' => $request->subject,
+        //     'Html-part' => "Feedback From: <strong>$request->email</strong><br/><br/>
+        //         $request->message <hr/>
+        //         <strong>SENT VIA
+        //         <a href='".env('APP_URL')."'>Cacao - Beyond Chocolate</a></strong>
+        //         <br/>",
+        //     'Recipients' => $recipients
+        // ];
+
+        // $response = false;
+        // try {
+        //     $response =  $mailjet->post(Resources::$Email, ['body' => $body]);
+        //     $response = $response->success();
+        // } catch (\Exception $e) {
+        //     logger()->error('Goutte client error ' . $e->getMessage());
+        // }
+        // return ['sent' => $response];
+
     }
     //
 }
