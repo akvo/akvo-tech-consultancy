09c217c0c90786141f6c51d5d17ec6a4de708784
zuhdil <zuhdil@gmail.com>
2020-11-30 15:09:46 +0700

    [#297] add missing mailjet config

--- sites/beyond-chocolate/config/mail.php
+++ sites/beyond-chocolate/config/mail.php
@@ -62,6 +62,10 @@ return [
             'path' => '/usr/sbin/sendmail -bs',
         ],
 
+        'mailjet' => [
+            'transport' => 'mailjet',
+        ],
+
         'log' => [
             'transport' => 'log',
             'channel' => env('MAIL_LOG_CHANNEL'),
