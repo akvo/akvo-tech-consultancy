eea22ec5a1c9ec485f14fe7239e183bfd2545e9a
wayangalihpratama <wgprtm@gmail.com>
2020-12-03 16:14:38 +0800

    [#338] Add sync.sh

---
+++ sites/2scale/sync.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# FOLDER="2scale-test" 
+FOLDER="2scale"
+
+rsync \
+    --archive \
+    --compress \
+    --progress \
+    --exclude=ci \
+    --exclude=node_modules \
+    . siteground:/home/customer/www/tc.akvo.org/public_html/$FOLDER/
+
+echo "Fixing permissions..."
+
+ssh siteground "find www/tc.akvo.org/public_html/${FOLDER}/ -not -path "*.well-known*" -type f -print0 | xargs -0 -n1 chmod 644"
+
+ssh siteground "find www/tc.akvo.org/public_html/${FOLDER}/ -type d -print0 | xargs -0 -n1 chmod 755"
+
+ssh siteground "cp ~/env/${FOLDER}.env.prod www/tc.akvo.org/public_html/${FOLDER}/.env"
+
+echo "Done"
