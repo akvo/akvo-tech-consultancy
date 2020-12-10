aa8ad56d892f92c435b6b846ac464ecf9f75ddf7
wayangalihpratama <wgprtm@gmail.com>
2020-12-10 10:27:01 +0800

    [#344, #337] Setup new project info pop up in german

--- sites/beyond-chocolate/resources/js/static/webform-content.js
+++ sites/beyond-chocolate/resources/js/static/webform-content.js
@@ -17,11 +17,11 @@ export const wfc = (handleShow) => {
         de: {
             dataSecurityText: <Fragment><a onClick={handleShow} href="#">Datensicherheitsvorkehrungen</a>  für die Daten, die Sie im Rahmen dieser Erhebung eingeben.</Fragment>,
             newProjectPopupText: {
-                section: 'A cocoa sustainability project is defined as a programme, project, or initiative targeting (aspects of) sustainability in cocoa production, processing, and/or supply chains. Please note that:',
+                section: 'Ein Kakao-Nachhaltigkeitsprojekt ist definiert als ein Programm, ein Projekt oder eine Initiative, die auf (Aspekte der) Nachhaltigkeit in der Kakaoproduktion, -verarbeitung und / oder in den Lieferketten abzielt. Bitte beachte, dass:',
                 list: [
-                    "You can register multiple cocoa sustainability projects (programmes/ projects/ initiatives) and report on these separately. This is done by opening a new project questionnaire for each project.",
-                    "You can choose to report on all your projects as one (umbrella) programme, thus reporting aggregated data.",
-                    'For each project you can specify the geographical intervention areas, this in "repeat groups" per country.'
+                    "Sie können mehrere Kakao-Nachhaltigkeitsprojekte (Programme / Projekte / Initiativen) registrieren und separat darüber berichten. Dazu öffnen Sie für jedes Projekt einen neuen Projektfragebogen.",
+                    "Sie können alle Ihre Projekte zu ein (Dach-) Programm zusammenfassen und so aggregierte Daten rapportieren. Öffnen Sie in diesem Fall nur einen Projekt-Fragebogen und nutzen diesen für die aggregierten Daten.",
+                    'Für jedes Projekt können Sie die Projektgebiete innerhalb eines Landes spezifizieren, in dem Sie die "repeat group"(Gruppe wiederholen)-Funktion (oben rechts) pro Land nutzen.'
                 ]
             }
         },
