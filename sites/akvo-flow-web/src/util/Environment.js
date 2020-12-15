7cc8f37ba33c3b1732808208ea3b8cd47d0a217e
dedenbangkit <mail@dedenbangkit.com>
2020-12-15 21:56:51 +0700

    [#358] Change c-retail form id

--- sites/akvo-flow-web/src/util/Environment.js
+++ sites/akvo-flow-web/src/util/Environment.js
@@ -20,7 +20,7 @@ export const SAVE_FEATURES = [
         api: "gisco-demo.tc.akvo.org/api",
         formEndpoint: false,
         skipPassword: true,
-        skipMandatories: ["111510043", "113130042", "116950964","111890828","134210832"],
+        skipMandatories: ["111510043", "113130042", "105640815","111890828","134210832"],
     },
 ];
 
