4d4dd62e4847d356795a3aea3e084426e2c78ec7
dedenbangkit <mail@dedenbangkit.com>
2020-12-17 00:05:02 +0700

    Await for fetch new cascade

--- sites/akvo-flow-web/src/util/Api.js
+++ sites/akvo-flow-web/src/util/Api.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 import { API_URL } from './Environment'
 
 export const getApi = (url) => new Promise((resolve, reject) => {
+    const now = new Date();
     let data = sessionStorage.getItem(url);
-    if (data === null) {
+    let cachetime = sessionStorage.getItem('cache-time');
+    cachetime = cachetime !== null
+        ? new Date(parseInt(cachetime) + (1 * 60 * 1000))
+        : new Date(0);
+    if (data === null || now > cachetime) {
         axios.get(API_URL + url).then(res => {
+            sessionStorage.setItem('cache-time', now.getTime());
             sessionStorage.setItem(url, JSON.stringify(res.data));
             resolve(res.data);
         })
     }
-    if (data !== null) {
+    if (data !== null && now < cachetime) {
         resolve(JSON.parse(data));
     }
 });
