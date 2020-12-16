929865489f262a0ce12ad51acfb3153db79c499f
dedenbangkit <mail@dedenbangkit.com>
2020-12-16 16:40:54 +0700

    Change deployment with two different branch

--- sites/beyond-chocolate/ci/deploy-semaphore.sh
+++ sites/beyond-chocolate/ci/deploy-semaphore.sh
@@ -10,7 +10,7 @@ sudo chown "${USER}:" . -R
 
 echo "Deploying site..."
 
-if [[ "${TRAVIS_BRANCH}" != "master" && "${TRAVIS_BRANCH}" != "develop" && "${TRAVIS_BRANCH}" != "sites/beyond-chocolate" ]]; then
+if [["${TRAVIS_BRANCH}" != "sites/beyond-chocolate" && "${TRAVIS_BRANCH}" != "sites/beyond-chocolate-test"]]; then
     exit 0
 fi
 
@@ -18,66 +18,60 @@ if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     exit 0
 fi
 
-FOLDERS="gisco-demo gisco-pilot"
-
-if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-	FOLDERS="gisco-demo gisco-pilot"
+FOLDER="gisco-demo"
+if [[ "${TRAVIS_BRANCH}" == "sites/beyond-chocolate" ]]; then
+	FOLDER="gisco-pilot"
 	echo "Deploying Production"
-else
-	echo "Deploying Test"
 fi
 
-for FOLDER in $FOLDERS
-do
-    echo "===================================================="
-    echo "START SYNC ${FOLDER}"
-    rsync \
-        --archive \
-        --compress \
-        --progress \
-        --exclude=ci \
-        --exclude=node_modules \
-        --rsh="ssh -i ${SITES_SSH_KEY} -o BatchMode=yes -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-        . u7-nnfq7m4dqfyx@35.214.170.100:/home/customer/www/tc.akvo.org/public_html/$FOLDER/
+echo "===================================================="
+echo "START SYNC ${FOLDER}"
+rsync \
+    --archive \
+    --compress \
+    --progress \
+    --exclude=ci \
+    --exclude=node_modules \
+    --rsh="ssh -i ${SITES_SSH_KEY} -o BatchMode=yes -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+    . u7-nnfq7m4dqfyx@35.214.170.100:/home/customer/www/tc.akvo.org/public_html/$FOLDER/
 
-    echo "Fixing permissions..."
+echo "Fixing permissions..."
 
-    ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
-        -p 18765 \
-        -o UserKnownHostsFile=/dev/null \
-        -o StrictHostKeyChecking=no \
-        u7-nnfq7m4dqfyx@35.214.170.100 "find www/tc.akvo.org/public_html/${FOLDER}/ -not -path "*.well-known*" -type f -print0 | xargs -0 -n1 chmod 644"
+ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    u7-nnfq7m4dqfyx@35.214.170.100 "find www/tc.akvo.org/public_html/${FOLDER}/ -not -path "*.well-known*" -type f -print0 | xargs -0 -n1 chmod 644"
 
-    ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
-        -p 18765 \
-        -o UserKnownHostsFile=/dev/null \
-        -o StrictHostKeyChecking=no \
-        u7-nnfq7m4dqfyx@35.214.170.100 "find www/tc.akvo.org/public_html/${FOLDER}/ -type d -print0 | xargs -0 -n1 chmod 755"
+ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    u7-nnfq7m4dqfyx@35.214.170.100 "find www/tc.akvo.org/public_html/${FOLDER}/ -type d -print0 | xargs -0 -n1 chmod 755"
 
-    echo "Copy the config..."
+echo "Copy the config..."
 
-    ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
-        -p 18765 \
-        -o UserKnownHostsFile=/dev/null \
-        -o StrictHostKeyChecking=no \
-        u7-nnfq7m4dqfyx@35.214.170.100 "cp ~/env/${FOLDER}.env.prod www/tc.akvo.org/public_html/${FOLDER}/.env"
+ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    u7-nnfq7m4dqfyx@35.214.170.100 "cp ~/env/${FOLDER}.env.prod www/tc.akvo.org/public_html/${FOLDER}/.env"
 
-    echo "Clearing cache..."
+echo "Clearing cache..."
 
-    ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
-        -p 18765 \
-        -o UserKnownHostsFile=/dev/null \
-        -o StrictHostKeyChecking=no \
-        u7-nnfq7m4dqfyx@35.214.170.100 "cd www/tc.akvo.org/public_html/${FOLDER}/ && /usr/local/bin/php73 artisan cache:clear"
+ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    u7-nnfq7m4dqfyx@35.214.170.100 "cd www/tc.akvo.org/public_html/${FOLDER}/ && /usr/local/bin/php73 artisan cache:clear"
 
-    echo "Migrating database - ${FOLDER}"
+echo "Migrating database - ${FOLDER}"
 
-    ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
-        -p 18765 \
-        -o UserKnownHostsFile=/dev/null \
-        -o StrictHostKeyChecking=no \
-        u7-nnfq7m4dqfyx@35.214.170.100 "cd www/tc.akvo.org/public_html/${FOLDER}/ && /usr/local/bin/php73 artisan migrate --force --no-interaction"
+ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    u7-nnfq7m4dqfyx@35.214.170.100 "cd www/tc.akvo.org/public_html/${FOLDER}/ && /usr/local/bin/php73 artisan migrate --force --no-interaction"
 
-    echo "===================================================="
-    echo "Done deploying ${FOLDER}"
-done
+echo "===================================================="
+echo "Done deploying ${FOLDER}"
