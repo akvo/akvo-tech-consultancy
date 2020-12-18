c4dfd4676f254cebaa6b752cdb15256f224811bf
wayangalihpratama <wgprtm@gmail.com>
2020-12-18 15:37:52 +0800

    [#360] Refactor impressum page and content code

--- sites/beyond-chocolate/resources/js/pages/Impressum.js
+++ sites/beyond-chocolate/resources/js/pages/Impressum.js
@@ -1,19 +1,65 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Accordion, Card } from "react-bootstrap";
 import { useLocale } from "../lib/locale-context";
 import { ic } from "../static/impressum-content";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faAngleRight,
+    faAngleDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Impressum = () => {
+    const [activeId, setActiveId] = useState(0);
     const { locale } = useLocale();
     let content = ic[locale.active];
+
+    const toggleActive = (id) => {
+        if (activeId === id) {
+            setActiveId(null);
+        } else {
+            setActiveId(id);
+        }
+    };
+
+    const renderImpressum = () => {
+        let data = ic[locale.active];
+        return data.list.map((x,idx) => {
+            return (
+                <Card key={"ic-"+idx}>
+                    <Accordion.Toggle 
+                        as={Card.Header} 
+                        eventKey={"ic-" + idx} 
+                        style={{cursor:"pointer"}} 
+                        className="faqList" 
+                        onClick={()=> toggleActive(idx)}
+                    >
+                        <h5 className={activeId === idx ? "green" : ""}>
+                            <FontAwesomeIcon
+                                className="mr-2"
+                                icon={activeId === idx ? faAngleDown : faAngleRight}
+                            /> {x.h}
+                        </h5>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey={"ic-" + idx}>
+                        <Card.Body className="faqList">
+                            {x.c}
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            )
+        });
+    };
 
     return (
         <Container fluid>
             <Row>
-                <Col className="mx-auto" md="8">
+                <Col className="mx-auto" md="9">
                     <h3>{ content.t }</h3>
                     <hr/>
-                    { content.c }
+                    {/* { content.c } */}
+                    <Accordion defaultActiveKey={"ic-" + activeId}>
+                        {renderImpressum()}
+                    </Accordion>
                 </Col>
             </Row>
         </Container>
--- sites/beyond-chocolate/resources/js/static/impressum-content.js
+++ sites/beyond-chocolate/resources/js/static/impressum-content.js
@@ -1,188 +1,169 @@
 import React, { Component, Fragment  } from "react";
 import { Accordion, Card } from "react-bootstrap";
 
+const icEn = {
+    t: "Impressum",
+    list: [
+        {
+            h: "Responsible for the content",
+            c: <Fragment>
+                    <h5>German Initiative on Sustainable Cocoa (GISCO)</h5> <hr/>
+                    <p>
+                        Secretariat Berlin <br/>
+                        c/o Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ) GmbH <br/>
+                        Reichpietschufer 20 <br/>
+                        10785 Berlin <br/>
+                        Germany <br/>
+                    </p>
+                    <p>
+                        Secretariat Eschborn <br/>
+                        Postfach 5180 <br/>
+                        65726 Eschborn <br/>
+                        Germany <br/>
+                    </p>
+                    <p>
+                        Contact: Beate Weiskopf <br/>
+                        Email: <a href="mailto:Beate.Weiskopf@giz.de">Beate.Weiskopf@giz.de</a> <br/>
+                    </p>
+                    <p>
+                        Beyond Chocolate <br/>
+                        Fosbury & Sons, Albert <br/>
+                        Koning Albert II Laan 7 <br/>
+                        1210 Brussels <br/>
+                    </p>
+                    <p>
+                        Contact: Charles Snoeck <br/>
+                        Email: <a href="mailto:snoeck@idhtrade.org">snoeck@idhtrade.org</a> <br/>
+                    </p>
+                </Fragment>
+        },
+        {
+            h: "Responsible for the technical realization",
+            c: <Fragment>
+                    <h5>Akvo Foundation</h5> <hr/>
+                    <p>
+                        's-Gravenhekje 1-A,  <br/>
+                        1011 TG Amsterdam, <br/>
+                        The Netherlands <br/>
+                    </p>
+                    <p>
+                        Phone: <br/>
+                        +31 20 820 0175 <br/>
+                    </p>
+                    <p>
+                        E-mail: <br/>
+                        <a href="mailto:">info@akvo.org</a> <br/>
+                    </p>
+                    <p>
+                        Netherlands Chamber of Commerce (KvK) number: 27327087 <br/>
+                        VAT number: NL 819794727 B01 <br/>
+                    </p>
+                    <p>
+                        Akvo Foundation does not commit to nor is it obliged to participate in the alternative dispute resolution for consumer disputes in front of a consumer dispute resolution entity. <br/>
+                        European Commission official website for Online Dispute Resolution: <a href="http://www.ec.europa.eu/consumers/odr">http://www.ec.europa.eu/consumers/odr</a><br/>
+                    </p>
+                    <p>
+                        Data privacy: <br/>
+                        Akvo's data privacy <a href="https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf">https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf</a>
+                    </p>
+                    <p>
+                        How can you request removal of your personal data from our systems? <br/>
+                        Please fill out this form (<a href="https://akvo.org/remove-my-personal-data/">https://akvo.org/remove-my-personal-data/</a>) if you'd like to have your personal data removed from our systems or write to our data security officer: <br/>
+                    </p>
+                    <p>
+                        Valeria Rogatchevskikh <br/>
+                        Email: <a href="mailto:">privacy@akvo.org</a> <br/> 
+                    </p>
+                    <p>
+                        You can also contact the above named data security officer to file a complaint. <br/>
+                    </p>
+                </Fragment>
+        }
+    ],
+};
+
+const icDe = {
+    t: "Impressum",
+    list: [
+        {
+            h: "Verantwortlich für den Inhalt",
+            c: <Fragment>
+                    <h5>Forum Nachhaltiger Kakao e.V</h5> <hr/>
+                    <p>
+                        Geschäftsstelle Berlin <br/>
+                        c/o Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ) GmbH <br/>
+                        Reichpietschufer 20 <br/>
+                        10785 Berlin <br/>
+                    </p>
+                    <p>
+                        Geschäftsstelle Eschborn <br/>
+                        Postfach 5180 <br/>
+                        65726 Eschborn <br/>
+                    </p>
+                    <p>
+                        Kontakt: Beate Weiskopf <br/>
+                        Email: <a href="mailto:Beate.Weiskopf@giz.de">Beate.Weiskopf@giz.de</a> <br/>
+                    </p>
+                    <p>
+                        Beyond Chocolate <br/>
+                        Fosbury & Sons, Albert <br/>
+                        Koning Albert II Laan 7 <br/>
+                        1210 Brussels <br/>
+                    </p>
+                    <p>
+                        Kontakt: Charles Snoeck <br/>
+                        Email: <a href="mailto:snoeck@idhtrade.org">snoeck@idhtrade.org</a> <br/>
+                    </p>
+                </Fragment>
+        },
+        {
+            h: "Verantwortlich für die technische Umsetzung",
+            c: <Fragment>
+                    <h4>Stiftung Akvo</h4> <hr/>
+                    <p>
+                        's-Gravenhekje 1-A,  <br/>
+                        1011 TG Amsterdam, <br/>
+                        Die Niederlande <br/>
+                    </p>
+                    <p>
+                        Telefon: <br/>
+                        +31 20 820 0175 <br/>
+                    </p>
+                    <p>
+                        E-mail: <br/>
+                        <a href="mailto:">info@akvo.org</a> <br/>
+                    </p>
+                    <p>
+                        Niederländische Handelskammer (KvK) Nummer: 27327087 <br/>
+                        Umsatzsteueridentifikationsnummer: NL 819794727 B01 <br/>
+                    </p>
+                    <p>
+                        Die Akvo Foundation ist weder verpflichtet noch daran gebunden, an einer alternativen Streitbeilegung für Verbraucherstreitigkeiten vor einer Verbraucherschlichtungsstelle teilzunehmen. <br/>
+                        Offizielle Website der Europäischen Kommission zur Online-Streitbeilegung: <a href="http://www.ec.europa.eu/consumers/odr">http://www.ec.europa.eu/consumers/odr</a><br/>
+                    </p>
+                    <p>
+                        Datenschutz: <br/>
+                        Akvo's Datenschutz  <a href="https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf">https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf</a>
+                    </p>
+                    <p>
+                        Wie können Sie die Löschung Ihrer persönlichen Daten aus unseren Systemen beantragen? <br/>
+                        Bitte füllen Sie dieses Formular (<a href="https://akvo.org/remove-my-personal-data/">https://akvo.org/remove-my-personal-data/</a>) aus, wenn Sie Ihre persönlichen Daten aus unseren Systemen entfernen lassen möchten oder schreiben Sie an unseren Datenschutzbeauftragten: <br/>
+                    </p>
+                    <p>
+                        Valeria Rogatchevskikh <br/>
+                        Email: <a href="mailto:">privacy@akvo.org</a> <br/>
+                    </p>
+                    <p>
+                        Sie können sich auch an die oben genannte Datenschutzbeauftragte wenden, um eine Beschwerde einzureichen. <br/>
+                    </p>
+                </Fragment>
+        }
+    ],
+};
+
 const ic = {
-    en: {
-        t: "Impressum",
-        c: <Fragment>
-                <Accordion defaultActiveKey="0">
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="0" style={{cursor:"pointer"}}>
-                            <h4>Responsible for the content</h4>
-                        </Accordion.Toggle>
-                        <Accordion.Collapse eventKey="0">
-                            <Card.Body>
-                                <h4>German Initiative on Sustainable Cocoa (GISCO)</h4> <hr/>
-                                <Card.Text>
-                                    Secretariat Berlin <br/>
-                                    c/o Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ) GmbH <br/>
-                                    Reichpietschufer 20 <br/>
-                                    10785 Berlin <br/>
-                                    Germany <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Secretariat Eschborn <br/>
-                                    Postfach 5180 <br/>
-                                    65726 Eschborn <br/>
-                                    Germany <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Contact: Beate Weiskopf <br/>
-                                    Email: <a href="mailto:Beate.Weiskopf@giz.de">Beate.Weiskopf@giz.de</a> <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Beyond Chocolate <br/>
-                                    Fosbury & Sons, Albert <br/>
-                                    Koning Albert II Laan 7 <br/>
-                                    1210 Brussels <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Contact: Charles Snoeck <br/>
-                                    Email: <a href="mailto:snoeck@idhtrade.org">snoeck@idhtrade.org</a> <br/>
-                                </Card.Text>
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="1" style={{cursor:"pointer"}}>
-                            <h4>Responsible for the technical realization</h4>
-                        </Accordion.Toggle>
-                        <Accordion.Collapse eventKey="1">
-                            <Card.Body>
-                                <h4>Akvo Foundation</h4> <hr/>
-                                <Card.Text>
-                                    's-Gravenhekje 1-A,  <br/>
-                                    1011 TG Amsterdam, <br/>
-                                    The Netherlands <br/>
-                                </Card.Text>
-                                <p>
-                                    Phone: <br/>
-                                    +31 20 820 0175 <br/>
-                                </p>
-                                <p>
-                                    E-mail: <br/>
-                                    <a href="mailto:">info@akvo.org</a> <br/>
-                                </p>
-                                <p>
-                                    Netherlands Chamber of Commerce (KvK) number: 27327087 <br/>
-                                    VAT number: NL 819794727 B01 <br/>
-                                </p>
-                                <Card.Text>
-                                    Akvo Foundation does not commit to nor is it obliged to participate in the alternative dispute resolution for consumer disputes in front of a consumer dispute resolution entity. <br/>
-                                    European Commission official website for Online Dispute Resolution: <a href="http://www.ec.europa.eu/consumers/odr">http://www.ec.europa.eu/consumers/odr</a><br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Data privacy: <br/>
-                                    Akvo's data privacy <a href="https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf">https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf</a>
-                                </Card.Text>
-                                <p>
-                                    How can you request removal of your personal data from our systems? <br/>
-                                    Please fill out this form (<a href="https://akvo.org/remove-my-personal-data/">https://akvo.org/remove-my-personal-data/</a>) if you'd like to have your personal data removed from our systems or write to our data security officer: <br/>
-                                </p>
-                                <p>
-                                    Valeria Rogatchevskikh <br/>
-                                    Email: <a href="mailto:">privacy@akvo.org</a> <br/> 
-                                </p>
-                                <p>
-                                    You can also contact the above named data security officer to file a complaint. <br/>
-                                </p>
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                </Accordion>
-            </Fragment>,
-    },
-
-    de: {
-        t: "Impressum",
-        c: <Fragment>
-                <Accordion defaultActiveKey="0">
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="0" style={{cursor:"pointer"}}>
-                            <h4>Verantwortlich für den Inhalt</h4>
-                        </Accordion.Toggle>
-                        <Accordion.Collapse eventKey="0">
-                            <Card.Body>
-                                <h4>Forum Nachhaltiger Kakao e.V</h4> <hr/>
-                                <Card.Text>
-                                    Geschäftsstelle Berlin <br/>
-                                    c/o Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ) GmbH <br/>
-                                    Reichpietschufer 20 <br/>
-                                    10785 Berlin <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Geschäftsstelle Eschborn <br/>
-                                    Postfach 5180 <br/>
-                                    65726 Eschborn <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Kontakt: Beate Weiskopf <br/>
-                                    Email: <a href="mailto:Beate.Weiskopf@giz.de">Beate.Weiskopf@giz.de</a> <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Beyond Chocolate <br/>
-                                    Fosbury & Sons, Albert <br/>
-                                    Koning Albert II Laan 7 <br/>
-                                    1210 Brussels <br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Kontakt: Charles Snoeck <br/>
-                                    Email: <a href="mailto:snoeck@idhtrade.org">snoeck@idhtrade.org</a> <br/>
-                                </Card.Text>
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="1" style={{cursor:"pointer"}}>
-                            <h4>Verantwortlich für die technische Umsetzung</h4>
-                        </Accordion.Toggle>
-                        <Accordion.Collapse eventKey="1">
-                            <Card.Body>
-                                <h4>Stiftung Akvo</h4> <hr/>
-                                <Card.Text>
-                                    's-Gravenhekje 1-A,  <br/>
-                                    1011 TG Amsterdam, <br/>
-                                    Die Niederlande <br/>
-                                </Card.Text>
-                                <p>
-                                    Telefon: <br/>
-                                    +31 20 820 0175 <br/>
-                                </p>
-                                <p>
-                                    E-mail: <br/>
-                                    <a href="mailto:">info@akvo.org</a> <br/>
-                                </p>
-                                <p>
-                                    Niederländische Handelskammer (KvK) Nummer: 27327087 <br/>
-                                    Umsatzsteueridentifikationsnummer: NL 819794727 B01 <br/>
-                                </p>
-                                <Card.Text>
-                                    Die Akvo Foundation ist weder verpflichtet noch daran gebunden, an einer alternativen Streitbeilegung für Verbraucherstreitigkeiten vor einer Verbraucherschlichtungsstelle teilzunehmen. <br/>
-                                    Offizielle Website der Europäischen Kommission zur Online-Streitbeilegung: <a href="http://www.ec.europa.eu/consumers/odr">http://www.ec.europa.eu/consumers/odr</a><br/>
-                                </Card.Text>
-                                <Card.Text>
-                                    Datenschutz: <br/>
-                                    Akvo's Datenschutz  <a href="https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf">https://akvo.org/wp-content/uploads/2018/05/Akvo-General-Privacy-policy-FINAL_May18_V1.pdf</a>
-                                </Card.Text>
-                                <p>
-                                    Wie können Sie die Löschung Ihrer persönlichen Daten aus unseren Systemen beantragen? <br/>
-                                    Bitte füllen Sie dieses Formular (<a href="https://akvo.org/remove-my-personal-data/">https://akvo.org/remove-my-personal-data/</a>) aus, wenn Sie Ihre persönlichen Daten aus unseren Systemen entfernen lassen möchten oder schreiben Sie an unseren Datenschutzbeauftragten: <br/>
-                                </p>
-                                <p>
-                                    Valeria Rogatchevskikh <br/>
-                                    Email: <a href="mailto:">privacy@akvo.org</a> <br/>
-                                </p>
-                                <p>
-                                    Sie können sich auch an die oben genannte Datenschutzbeauftragte wenden, um eine Beschwerde einzureichen. <br/>
-                                </p>
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                </Accordion>
-            </Fragment>,
-    }
+    en: icEn,
+    de: icDe,
 };
 
 export { ic };
