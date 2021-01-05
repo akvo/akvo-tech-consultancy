1cdd84dfe391e9ceb620bffb13519ec727dfa649
wayangalihpratama <wgprtm@gmail.com>
2021-01-05 21:26:42 +0800

    [#344] Update data security provision content

--- sites/beyond-chocolate/resources/js/static/data-security-content.js
+++ sites/beyond-chocolate/resources/js/static/data-security-content.js
@@ -13,7 +13,7 @@ const dsc = {
 
             <h4>Data security & privacy</h4>
             <p className="text-justify">
-                Akvo maintains strong data security and privacy protection policies, frameworks, and procedures. Data is stored securely; we ensure data is safely encrypted, and your data is never shared with third parties. ​Imported data is owned by you. We comply with the European General Data Protection Regulations (GDPR).
+                Akvo maintains strong data security and privacy protection policies, frameworks, and procedures, as laid out further in the below text. Data is stored securely; we ensure data is safely encrypted, and your data is never shared with third parties. ​ We comply with the European General Data Protection Regulations (GDPR).
             </p>
             <br/>
 
@@ -35,6 +35,15 @@ const dsc = {
             </p>
             <br/>
 
+            <h4>Data processing by Akvo</h4>
+            <p className="text-justify">
+                The Processor entrusts only such persons (whether legal or natural) with the data Processing under this Agreement who have given an undertaking to maintain confidentiality and have been informed of any special data protection requirements relevant to their work; Akvo acts as a data processor on behalf of Beyond Chocolate and GISCO.
+            </p>
+            <p className="text-justify">
+                The data collected are subject to the GDPR which is a EU law that protects personal data, this is also stated in Akvo’s Data Processing Agreement: <a href="https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf">https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf</a>
+            </p>
+            <br/>
+
             <h4>Roles and permissions</h4>
             <p className="text-justify">
                 The platform secretariats of Beyond Chocolate, SWISSCO and GISCO will follow a roles and permissions model, allowing for administrator level and also granular access control to data. Access to questions and attendant response data will be restricted to a user’s specific and defined role and the permissions assigned to that role. A strict permissions model ensures that sensitive data is not accessible nor visible to other users of the platforms.
@@ -46,7 +55,7 @@ const dsc = {
                 Before publishing (anonymized and aggregated) findings or insights arising from the analysis of collected data, the platform secretariats will still ensure that this does not indirectly disclose sensitive information with respect to a particular Member.
             </p>
             <p className="text-justify">
-                Access to data before anonymization is possible only for a very limited number of duly authorized persons having signed non-disclosure commitments; this may solely be done to check for and ensure consistency in data;  getting back to the members to correct for erroneous or contradicting data and/or for providing support to gradual improvement of sustainability reporting by Members. 
+                Access to data before anonymization is possible only for a very limited number of duly authorized persons having signed non-disclosure commitments; this may solely be done to check for and ensure consistency in data; getting back to the members to correct for erroneous or contradicting data and/or for providing support to gradual improvement of sustainability reporting by Members.
             </p>
             <p className="text-justify">
                 The following persons will have such access to data before anonymization and aggregation:
@@ -79,13 +88,22 @@ const dsc = {
             <p className="text-justify">
                 Each of the named persons above is required to sign a confidentiality agreement before getting access to the data of the corresponding platform members. Any new personnel at the secretariats or at C-Lever.org that needs to be granted access will have to have signed the confidentiality agreement before getting such access.
             </p>
+            <br/>
 
-            <h4>Data processing by Akvo</h4>
+            <h4>Handling of sensitive data after anonymization and aggregation of collected data</h4>
             <p className="text-justify">
-                The Processor entrusts only such persons (whether legal or natural) with the data Processing under this Agreement who have given an undertaking to maintain confidentiality and have been informed of any special data protection requirements relevant to their work; Akvo acts as a data processor on behalf of Beyond Chocolate and GISCO.
+                When reporting or sharing any data, compiled from data collected with the monitoring tool, appropriate measures will be taken to ensure that the nature of the reported data do not allow any conclusions on specific members. Therefore, the secretariats will follow the following principles:
             </p>
+            <ol className="pl-5">
+                <li>No only ensure that data are anonymised, but also aggregated and/or averaged before any publication of data.</li>
+                <li>Still be careful in sharing anonymised, but also aggregated and/or averaged data and check where any risk of sharing sensitive information exists. In this case, the Members involved will be contacted first.</li>
+                <li>Internal reporting to the Members, in a confidential setting, will always precede external reporting.</li>
+            </ol>
+            <br/>
+
+            <h4>Agreement to and visibility of these data security provisions</h4>
             <p className="text-justify">
-                The data collected are subject to the GDPR which is a EU law that protects personal data, this is also stated in Akvo’s  Data Processing Agreement: <a href="https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf">https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf</a>
+                When registering, the users are asked to agree to their data being processed according to the above data security provisions by clicking a box. Also, the link to the data security provisions is shown at the top of each survey at all times. 
             </p>
         </Fragment>
     ,
@@ -93,7 +111,7 @@ const dsc = {
     de: <Fragment>
             <h4>Dateneigentum und -verfahren</h4>
             <p className="text-justify">
-                Der Tool-Entwickler Akvo beansprucht kein Dateneigentum. Alle Daten, die über das System erfasst werden, sind rechtlich Eigentum der Organisationen, die die Daten eingeben. Alle Rechte verbleiben bei den Organisationen, denen die Daten gehören. Akvo-Systeme ermöglichen die Verwaltung bestimmter Rollen und Berechtigungenmit entsprechendem Zugriff auf Daten und / oder Funktionen im System.    
+                Der Tool-Entwickler Akvo beansprucht kein Dateneigentum. Alle Daten, die über das System erfasst werden, sind rechtlich Eigentum der Organisationen, die die Daten eingeben. Alle Rechte verbleiben bei den Organisationen, denen die Daten gehören. Akvo-Systeme ermöglichen die Verwaltung bestimmter Rollen und Berechtigungen mit entsprechendem Zugriff auf Daten und / oder Funktionen im System. 
             </p>
             <p className="text-justify">
                 Angesichts der möglichen Erweiterung des Systems im Laufe der Zeit planen wir separate Besprechungen, um die Rollen und Verantwortlichkeiten verschiedener Interessengruppen zu klären und Richtlinien und Datenverfahren für die Verwaltung und Implementierung dieser Rollen ,sowie den entsprechenden Zugriff auf Daten und / oder Funktionen im System festzulegen. 
@@ -102,7 +120,7 @@ const dsc = {
 
             <h4>Datensicherheit und Datenschutz</h4>
             <p className="text-justify">
-                Akvo hält strenge Richtlinien und Verfahren für Datensicherheit und Datenschutz ein. Daten werden sicher gespeichert. Wir stellen sicher, dass die Daten sicher verschlüsselt sind und Ihre Daten niemals an Dritte weitergegeben werden. Importierte Daten gehören Ihnen. Wir halten uns an die Europäischen Allgemeinen Datenschutzbestimmungen (DSGVO).
+                Akvo hält strenge Richtlinien und Verfahren für Datensicherheit und Datenschutz ein, welche im Folgenden dargestellt werden. Alle Daten werden sicher gespeichert. Wir stellen sicher, dass die Daten sicher verschlüsselt sind und Ihre Daten niemals an Dritte weitergegeben werden. Wir halten uns an die Europäischen Allgemeinen Datenschutzbestimmungen (DSGVO).
             </p>
             <br/>
 
@@ -124,9 +142,18 @@ const dsc = {
             </p>
             <br/>
 
+            <h4>Datenverarbeitung durch Akvo</h4>
+            <p className="text-justify">
+                Der Datenverarbeiter beauftragt nur Personen (ob legal oder natürlich) mit der Datenverarbeiten gemäß dieser Vereinbarung, die der Bewahrung der Datenvertraulichkeit zugestimmt haben und über besondere Datenschutzmaßnahmen in Bezug auf ihre Arbeit unterrichtet wurden. Akvo übernimmt die Rolle des Datenverarbeiters im Auftrag von Beyond Chocolate und dem Forum Nachhaltiger Kakao.
+            </p>
+            <p className="text-justify">
+                Die gespeicherten Daten unterliegen der DSGVO, dem europäischen Gesetz zum Schutz persönlicher Daten. Dies ist auch in Akvo`s Datenverarbeitungsbestimmung festgehalten: <a href="https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf">https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf</a>
+            </p>
+            <br/>
+
             <h4>Rollen und Berechtigungen</h4>
             <p className="text-justify">
-                Die Plattformsekretariate von Beyond Chocolate, SWISSCO und dem Forum Nachhaltiger Kakao folgen einem Rollen- und Berechtigungsmodell, das eine Administratorebene-, sowohl  eine detaillierte Zugriffskontrolle auf Daten ermöglicht. Der Zugriff auf Fragen und zugehörige Antwortsdaten ist auf die spezifische und definierte Rolle eines/r Benutzers/in und die dieser Rolle zugewiesenen Berechtigungen beschränkt. Ein striktes Berechtigungsmodell stellt sicher, dass vertrauliche Daten für andere Benutzer der Plattformen weder zugänglich noch sichtbar sind.
+                Die Plattformsekretariate von Beyond Chocolate, SWISSCO und dem Forum Nachhaltiger Kakao folgen einem Rollen- und Berechtigungsmodell, das eine Administratorebene-, sowohl eine detaillierte Zugriffskontrolle auf Daten ermöglicht. Der Zugriff auf Fragen und zugehörige Antwortsdaten ist auf die spezifische und definierte Rolle eines/r Benutzers/in und die dieser Rolle zugewiesenen Berechtigungen beschränkt. Ein striktes Berechtigungsmodell stellt sicher, dass vertrauliche Daten für andere Benutzer der Plattformen weder zugänglich noch sichtbar sind.
             </p>
             <p className="text-justify">
                 Sofern mit dem/r Eigentümer/in der Daten nicht ausdrücklich anders vereinbart, müssen die gesammelten Daten vollständig anonymisiert und aggregiert werden, bevor sie mit anderen Mitgliedern der Plattformen geteilt und / oder veröffentlicht werden können. Alle Daten, die intern auf disaggregierter Ebene geteilt und / oder angezeigt werden (z. B. mit den Mitarbeitern/innen der Sekretariate oder den Plattformarbeitsgruppen, die die Überwachung begleiten, einschließlich der Datenauswertung), werden vollständig anonymisiert.
@@ -168,13 +195,22 @@ const dsc = {
             <p className="text-justify">
                 Jede der oben genannten Personen muss eine Vertraulichkeitsvereinbarung unterzeichnen, bevor sie auf Daten der jeweiligen Plattformmitglieder zugreifen kann. Jede/r neue Mitarbeiter/in in den Sekretariaten oder auf C-Lever.org, dem/der Zugang gewährt wird, muss die Vertraulichkeitsvereinbarung unterzeichnet haben, bevor er/sie Zugang erhält.
             </p>
-
-            <h4>Datenverarbeitung durch Akvo</h4>
+            <br/>
+            
+            <h4>Umgang mit sensiblen Daten nach der Anonymisierung und Aggregierung erhobener Daten</h4>
             <p className="text-justify">
-                Der Datenverarbeiter beauftragt nur Personen (ob legal oder natürlich) mit der Datenverarbeiten gemäß dieser Vereinbarung, die der Bewahrung der Datenvertraulichkeit zugestimmt haben und über besondere Datenschutzmaßnahmen in Bezug auf ihre Arbeit unterrichtet wurden. Akvo übernimmt die Rolle des Datenverarbeiters im Auftrag von Beyond Chocolate und dem Forum Nachhaltiger Kakao.
+                Wann immer Daten, die mit dem Monitoringtool erhoben wurden, weitergegeben oder bekanntgemacht werden sollen, werden passende Vorkehrungen getroffen um sicherzustellen, dass aufgrund der Natur der Daten keine Rückschlüsse auf Mitglieder getroffen werden können. Dafür werden durch die Sekretariate die folgenden Prinzipien eingehalten:
             </p>
+            <ol className="pl-5">
+                <li>Es wird vor der Veröffentlichung von Daten nicht nur sichergestellt, dass Daten anonymisiert sind, sondern zusätzlich eine Aggregierung oder Durchschnittsbildung  erfahren haben.</li>
+                <li>Auch bei anonymisierten Daten und bei Aggregierungen oder Durchschnitten wird Vorsicht walten gelassen und wird überprüft ob ein Risiko bezüglich der Weitergabe sensibler Informationen besteht. In diesem Fall werden die betroffenen Mitglieder vor jedweder Weitergabe kontaktiert.</li>
+                <li>Daten werden vor einer externen Veröffentlichung immer zuerst unter vertraulichen Bedingungen intern innerhalb der Mitglieder der Plattformen geteilt.</li>
+            </ol>
+            <br/>
+
+            <h4>Zustimmung zu und Sichbarkeit der Datensicherheitsvorkehrungen</h4>
             <p className="text-justify">
-                Die gespeicherten Daten unterliegen der DSGVO, dem europäischen Gesetz zum Schutz persönlicher Daten. Dies ist auch in Akvo`s Datenverarbeitungsbestimmung festgehalten: <a href="https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf">https://akvo.org/wp-content/uploads/2020/06/Akvo-DPA-Template-revised-June-2020.pdf</a>
+                Bei der Registrierung werden die NutzerInnen gebeten, der Verarbeitung ihrer Daten gemäß den oben genannten Datensicherheitsvorkehrungen zuzustimmen, indem sie ein entsprechendes Feld anklicken. Der Link zu den Datensicherheitsvorkehrungen wird außerdem jederzeit oberhalb der Fragebögen angezeigt.
             </p>
         </Fragment>
     ,
