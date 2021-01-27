f2ed1c04850e48da47af6fcc5e1b1df5293fce01
wayangalihpratama <wgprtm@gmail.com>
2021-01-27 18:16:32 +0800

    [#353] Fix bugs on override verification email

--- sites/beyond-chocolate/app/Providers/AppServiceProvider.php
+++ sites/beyond-chocolate/app/Providers/AppServiceProvider.php
@@ -43,8 +43,7 @@ class AppServiceProvider extends ServiceProvider
         \Illuminate\Auth\Notifications\VerifyEmail::toMailUsing(function ($notifiable) {
             $verifyUrl = \URL::temporarySignedRoute(
                 'verification.verify',
-                \Illuminate\Support\Carbon::now()->addMinutes(\Illuminate\Support\Facades, 
-                \Config::get('auth.verification.expire', 60)),
+                \Illuminate\Support\Carbon::now()->addMinutes(\Illuminate\Support\Facades\Config::get('auth.verification.expire', 60)),
                 [
                     'id' => $notifiable->getKey(),
                     'hash' => sha1($notifiable->getEmailForVerification()),
