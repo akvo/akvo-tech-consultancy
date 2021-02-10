89c00695d2517d43f65b264ab661f7840368d49f
wayangalihpratama <wgprtm@gmail.com>
2021-02-10 21:29:44 +0800

    [#409] Redeploy

--- sites/idh-dashboard/resources/js/pages/CountryTab.js
+++ sites/idh-dashboard/resources/js/pages/CountryTab.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { connect } from "react-redux";
 import { mapStateToProps, mapDispatchToProps } from "../reducers/actions";
 import { useLocation } from 'react-router-dom';
