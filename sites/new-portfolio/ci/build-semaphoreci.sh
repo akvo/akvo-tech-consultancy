7f55017cf5695355898bc46564fd5ae6e6d4add6
zuhdil <zuhdil@gmail.com>
2021-02-11 09:57:12 +0700

    [#404] ci config

---
+++ sites/new-portfolio/ci/build-semaphoreci.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu
+
+function log {
+   echo "$(date +"%T") - INFO - $*"
+}
+
+export PROJECT_NAME=akvo-lumen
+
+if [ -z "$TRAVIS_COMMIT" ]; then
+    export TRAVIS_COMMIT=local
+fi
+
+log Creating Production image
+
+docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/tech-consultancy-new-portfolio:${TRAVIS_COMMIT} .
+
+log Done
