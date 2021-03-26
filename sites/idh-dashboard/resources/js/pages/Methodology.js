3a5cb5281c35747d3a811e573a0abc68c3657232
wayangalihpratama <wgprtm@gmail.com>
2021-03-26 20:13:36 +0800

    [#427] Update methodology file

--- sites/idh-dashboard/resources/js/pages/Methodology.js
+++ sites/idh-dashboard/resources/js/pages/Methodology.js
@@ -1,30 +1,29 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { mapStateToProps, mapDispatchToProps } from "../reducers/actions";
 import { auth } from "../data/api.js";
 import { Redirect } from "react-router-dom";
 
 class Methodology extends Component {
-    
     constructor(props) {
         super(props);
-        this.state={
-            redirect:false
-        }    
-    };
+        this.state = {
+            redirect: false
+        };
+    }
 
     componentDidMount() {
         const token = localStorage.getItem("access_token");
         if (token === null) {
             this.props.user.logout();
-            this.setState({redirect:true});
+            this.setState({ redirect: true });
         }
         if (token) {
             auth(token).then(res => {
                 const { status, message } = res;
                 if (status === 401) {
                     this.props.user.logout();
-                    this.setState({redirect:true});
+                    this.setState({ redirect: true });
                 }
                 return res;
             });
@@ -37,17 +36,18 @@ class Methodology extends Component {
         }
         return (
             <Fragment>
-                <iframe 
-                    src={"/files/IDH Farmfit Primary Data Collection Methodology (version 20-02-10).pdf"}
-                    style={{overflow:"hidden !important"}}
+                <iframe
+                    src={
+                        "/files/IDH Farmfit Primary Data Collection Methodology Dec 11 2020 updated.pdf"
+                    }
+                    style={{ overflow: "hidden !important" }}
                     frameBorder={0}
                     width={"100%"}
                     height={"916px"}
-                >
-                </iframe>
+                ></iframe>
             </Fragment>
         );
-    };
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Methodology);
