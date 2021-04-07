59767ae11d5858c72860f4b82949da4608a6be55
wayangalihpratama <wgprtm@gmail.com>
2021-04-07 09:00:44 +0800

    [#365] Update syngenta dataset

--- sites/idh-dashboard/config/data.php
+++ sites/idh-dashboard/config/data.php
@@ -5,54 +5,54 @@ $path = '/database/sources/';
 return [
     'notes' => 'https://docs.google.com/spreadsheets/d/1pk8GW-u6KGvTy7RFRtIGzRqIdvroazGJkF0CpYitSl0/edit#gid=0',
     'sources' => [
-        // [
-        //     'sid' => 84400281,
-        //     'fid' => 92080291,
-        //     'file' => $path . '2020-11-17_DD_UI_Input_mwea.csv',
-        //     'kind' => 'Rice',
-        //     'country' => 'Kenya',
-        //     'company' => 'Mwea Rice',
-        //     'case_number' => 86,
-        //     'cascade' => [
-        //         'name' => 'pi_location_cascade_county',
-        //     ],
-        //     'files' => [
-        //         ["type" => "raw", "text" => "Analyzed Mwea Rice Data", "to" => ".xlsx" ],
-        //     ],
-        //     'report_url' => '/files/Data-delivery-Mwea.html',
-        // ],
-        // [
-        //     'sid' => 66630001,
-        //     'fid' => 70650001,
-        //     'file' => $path . '2020-11-17_DD_UI_Input_egranary.csv',
-        //     'kind' => 'Maize',
-        //     'country' => 'Kenya',
-        //     'company' => 'E-granary',
-        //     'case_number' => 81,
-        //     'cascade' => [
-        //         'name' => 'pi_location_cascade_county',
-        //     ],
-        //     'files' => [
-        //         ["type" => "raw", "text" => "Analyzed E-granary Data", "to" => ".xlsx" ],
-        //     ],
-        //     'report_url' => '/files/Data-delivery-Egranary.html',
-        // ],
-        // [
-        //     'sid' => 147070008,
-        //     'fid' => 143920001,
-        //     'file' => $path . '2021-03-23_smart logistics_anom.csv',
-        //     'kind' => 'Beans',
-        //     'country' => 'Kenya',
-        //     'company' => 'Smart Logistics',
-        //     'case_number' => null,
-        //     'cascade' => [
-        //         'name' => 'pi_location_cascade_county',
-        //     ],
-        //     'files' => [
-        //         ["type" => "raw", "text" => "Analyzed Smart Logistics Data", "to" => ".xlsx" ],
-        //     ],
-        //     'report_url' => '',
-        // ],
+        [
+            'sid' => 84400281,
+            'fid' => 92080291,
+            'file' => $path . '2020-11-17_DD_UI_Input_mwea.csv',
+            'kind' => 'Rice',
+            'country' => 'Kenya',
+            'company' => 'Mwea Rice',
+            'case_number' => 86,
+            'cascade' => [
+                'name' => 'pi_location_cascade_county',
+            ],
+            'files' => [
+                ["type" => "raw", "text" => "Analyzed Mwea Rice Data", "to" => ".xlsx" ],
+            ],
+            'report_url' => '/files/Data-delivery-Mwea.html',
+        ],
+        [
+            'sid' => 66630001,
+            'fid' => 70650001,
+            'file' => $path . '2020-11-17_DD_UI_Input_egranary.csv',
+            'kind' => 'Maize',
+            'country' => 'Kenya',
+            'company' => 'E-granary',
+            'case_number' => 81,
+            'cascade' => [
+                'name' => 'pi_location_cascade_county',
+            ],
+            'files' => [
+                ["type" => "raw", "text" => "Analyzed E-granary Data", "to" => ".xlsx" ],
+            ],
+            'report_url' => '/files/Data-delivery-Egranary.html',
+        ],
+        [
+            'sid' => 147070008,
+            'fid' => 143920001,
+            'file' => $path . '2021-03-23_smart logistics_anom.csv',
+            'kind' => 'Beans',
+            'country' => 'Kenya',
+            'company' => 'Smart Logistics',
+            'case_number' => null,
+            'cascade' => [
+                'name' => 'pi_location_cascade_county',
+            ],
+            'files' => [
+                ["type" => "raw", "text" => "Analyzed Smart Logistics Data", "to" => ".xlsx" ],
+            ],
+            'report_url' => '',
+        ],
         [
             'sid' => 158095822,
             'fid' => 145035285,
