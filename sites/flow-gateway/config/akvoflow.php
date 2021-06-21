797fc8036b2ac2b6faa61fb4952e0a2ec7ec2680
wayangalihpratama <wgprtm@gmail.com>
2021-06-21 17:26:57 +0800

    [#483] Add fetch/update flow-api method to env and config

--- sites/flow-gateway/config/akvoflow.php
+++ sites/flow-gateway/config/akvoflow.php
@@ -1,11 +1,12 @@
 <?php
 
 $endpoint = env('FLOW_API',"https://tech-consultancy.akvotest.org/akvo-flow-web-api");
+$method = env('FLOW_API_METHOD', 'update');
 
 return [
     'instances' => ['2scale','seap', 'watershed', 'ssh4a'],
     'upload_image' => $endpoint.'/upload-image',
     'submit' => $endpoint.'/submit-form',
-    'form_api' => $endpoint.'/#instance_name/#form_id/fetch',
+    'form_api' => $endpoint.'/#instance_name/#form_id/'.$method,
     'cascade_api' => $endpoint.'/cascade/seap/#cascade/#level'
 ];
